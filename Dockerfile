f045ed73b6a65c11501d858cd573190484e0d7c9
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-09-28 21:24:39 +1300

    Deps update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14.10.1-slim
+FROM node:14.11.0-slim
 
 WORKDIR /app
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,18 @@
 <template>
-  <section class="text-gray-500 bg-black body-font h-full">
+  <section class="text-gray-500 body-font h-full">
     <div class="container mx-auto flex flex-col px-5 py-24 justify-center items-center">
       <div class="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
         <h1 class="title-font sm:text-4xl text-3xl mb-4 font-bold text-white">
           XANZHU.COM
         </h1>
-        <div class="font-landing mb-8 mt-2 mx-5 break-word text-ld leading-6 text-gray-300 sm:text-2xl">
-          <!-- <p>This page will constantly update as we improve our design</p> -->
-          <!-- <p>우리는 우리의 디자인을 개선으로이 페이지는 지속적으로 업데이트됩니다.</p> -->
-          <p>
-            随着我们改进设计 此页面将继续更改
-          </p>
-          <!-- <p>We expect to release our website by the end of the year</p> -->
-          <p>我们预计它将在今年年底发布</p>
-          <!-- <p>우리는 올해 말까지 출시 할 것으로 예상.</p> -->
+        <div class="mb-8 mt-2 mx-5 break-word text-sm leading-5 text-gray-300 sm:text-base">
+          <p>This page will constantly update as we improve our design</p>
+          <p>We expect to release our website by the end of the year</p>
+          <p>ETH 0xF74c5B6956F0B7674659323665Db90313f73557E</p>
         </div>
         <svg class="h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 260 260"><defs><style>.cls-1{fill:#f91249}.cls-2{fill:#e50e4b}.cls-3{fill:#ff1060}</style></defs><path class="cls-1" transform="rotate(-45 129.774 185.036)" d="M107.07 162.34h45.41v45.41h-45.41z" /><path class="cls-2" transform="rotate(-45 161.885 217.145)" d="M139.18 194.45h45.41v45.41h-45.41z" /><path class="cls-3" transform="rotate(-45 97.665 217.146)" d="M74.97 194.45h45.41v45.41H74.97z" /><path transform="rotate(135 129.515 74.138)" fill="#ff127e" d="M106.81 51.43h45.41v45.41h-45.41z" /><path class="cls-3" transform="rotate(135 97.41 42.03)" d="M74.71 19.33h45.41v45.41H74.71z" /><path class="cls-2" transform="rotate(135 161.623 42.033)" d="M138.92 19.33h45.41v45.41h-45.41z" /><path class="cls-3" transform="rotate(-135 185.787 129.23)" d="M163.08 106.53h45.41v45.41h-45.41z" /><path class="cls-1" transform="rotate(-135 217.892 97.122)" d="M195.19 74.42h45.41v45.41h-45.41z" /><path class="cls-2" transform="rotate(-135 217.895 161.334)" d="M195.19 138.63h45.41v45.41h-45.41z" /><path class="cls-2" transform="rotate(45 74.226 129.145)" d="M51.52 106.44h45.41v45.41H51.52z" /><path class="cls-1" transform="rotate(45 42.116 161.255)" d="M19.41 138.55h45.41v45.41H19.41z" /><path class="cls-3" transform="rotate(45 42.118 97.106)" d="M19.41 74.4h45.41v45.41H19.41z" /></svg>
       </div>
+      <a href="0xEECD76678d22700e700A8FcF3C4461735340d1cf" target="_blank" title="Donate ETH"><img src="https://etherdonation.com/i/btn/donate-btn.png" alt="Donate ETH"/></a>
     </div>
   </section>
 </template>
