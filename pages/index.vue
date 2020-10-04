e88446f867ce7417ec6565844542a352805daee5
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-10-04 14:06:34 +1300

    Minor changes

--- pages/index.vue
+++ pages/index.vue
@@ -16,19 +16,19 @@
           <p>This page will constantly update as we improve our design</p>
           <p>We expect to release our website by the end of the year</p>
         </div>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="isHidden = !isHidden">
-          Test
+        <button class="bg-yellow-600 hover:bg-white text-black font-bold py-2 px-4 rounded" @click="isHidden = !isHidden">
+          Donate
         </button>
         <div v-if="isHidden">
-          <p class="mt-4">
+          <p class="mt-4 text-white">
             Direct Address: ETH 0xF74c5B6956F0B7674659323665Db90313f73557E
           </p>
-          <a class="text-red-700" href="https://commerce.coinbase.com/checkout/fd080deb-7ead-4bba-a979-0292e7109e70">Coinbase donation</a>
+          <a class="text-red-600" href="https://commerce.coinbase.com/checkout/fd080deb-7ead-4bba-a979-0292e7109e70">Coinbase donation</a>
           <p />
         </div>
-        <!-- <p v-else class="mt-4 text-xs">
+        <p v-else class="mt-4 text-xs">
           ETH 0xF74c5B6956F0B7674659323665Db90313f73557E
-        </p> -->
+        </p>
       </div>
     </div>
   </section>
