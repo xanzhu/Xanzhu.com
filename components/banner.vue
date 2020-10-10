3c2e9684cf0dfd82a5244a38f38d0ffd5f3329eb
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-10-10 23:15:06 +1300

    Added featured text

--- components/banner.vue
+++ components/banner.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="h-56 bg-gradient-to-r from-blue-400 via-blue-700 to-blue-400">
-      <div class="bg-transparent">
-          <img class="h-56 w-full object-scale-down object-right" src="~/assets/banner-21.png">
+  <div class="h-56 bg-purple-900">
+    <div class="text-white absolute">
+      <div class="mx-20 my-16 bg-red leading-9 text-4xl antialiased font-medium">
+        Understand Security, Technology
+        <br>
+        and everything in-between.
       </div>
+    </div>
+    <div class="bg-transparent">
+      <!-- <img class="h-56 w-full object-cover object-right" src="~/assets/banner-21.png"> Option with cut-->
+      <img class="h-56 w-full object-cover" src="~/assets/banner-image.webp">
+    </div>
   </div>
 </template>
 <script>
