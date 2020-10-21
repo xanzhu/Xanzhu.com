bf207764738526808a3e4947ba9a3dbe15d054c0
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-10-21 13:25:12 +1300

    Improved structure

--- components/banner.vue
+++ components/banner.vue
@@ -1,15 +1,10 @@
 <template>
-  <div class="h-32 bg-gray-900">
+  <div class="h-32 bg-black">
     <div class="text-white absolute">
       <div class="mx-8 sm:mx-12 lg:mx-16 xl:mx-32 my-5 leading-9 text-4xl antialiased font-medium">
-        Understand Security, Technology
-        <br>
-        and everything in-between.
+        <span>Understand Security, Technology <br> and everything in-between.</span>
       </div>
     </div>
-    <!-- <div class="text-yellow-400 font-bold tracking-widest absolute right-0 m-40 text-6xl">
-      LATEST NEWS
-    </div> -->
     <div class="bg-transparent">
       <!-- <img class="h-56 w-full object-scale-down object-right" src="~/assets/banner-21.png"> Option with cut-->
       <img class="h-32 md:h-46 lg:60 w-full object-cover" src="~/assets/banner-image.webp">
