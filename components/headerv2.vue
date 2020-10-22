23cdb1a01e89fa344145c27abf6c4a4c32b50866
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-10-22 13:55:22 +1300

    Performance improvements * Cleaned up Mobile formats * Updated font weight and position

--- components/headerv2.vue
+++ components/headerv2.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="sm:flex sm:justify-between sm:mx-6 sm:py-2 sm:items-center sm:flex-row">
-    <div class="relative">
+    <div class="relative sm:block hidden">
       <button class="mt-2 text-gray-400 block h-8 w-8 focus:outline-none hover:text-white" @click="isOpen2 = !isOpen2">
         <svg class="stroke-current object-cover" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <xubanner />
-    <span class="text-red-600 py-2 text-sm font-semibold absolute mx-6 -mt-8 tracking-widest">{{ $t('TranslationWarn') }}</span>
+    <span class="text-red-600 py-2 text-ms sm:text-lg font-normal absolute mx-6 -mt-10 tracking-widest text-center">{{ $t('TranslationWarn') }}</span>
     <xunav />
     <div class="text-left text-gray-300 py-4 font-thin text-lg mx-10 sm:mx-0 sm:text-center sm:py-12">
       <p>This page will constantly update as we improve our design</p>
