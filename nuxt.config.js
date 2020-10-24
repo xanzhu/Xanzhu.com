ae3aa4f0077e8e108caa77734c67576604daed57
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-10-24 14:06:38 +1300

    Updated title

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
-    title: 'DUUNE BY XANZHU',
+    title: 'XANZHU | Developer preview',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
