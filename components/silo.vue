36a15723358478303f96bb4c097e681e46cbc726
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-11-19 14:27:50 +1300

    Mac Changes

--- components/silo.vue
+++ components/silo.vue
@@ -1,67 +1,34 @@
 <template>
-  <div class="text-white">
-    <!-- Translation Icon -->
-    <div class="sm:block hidden">
-      <button>
-        <TranslateIcon />
-      </button>
-      <!-- <div>
-        <nuxt-link
-          v-for="locale in availableLocales"
-          :key="locale.code"
-          :to="switchLocalePath(locale.code)"
-          class="text-black font-medium block px-4 hover:bg-gray-500 rounded-lg"
-        >
-          {{ locale.name }}
-        </nuxt-link>
-      </div> -->
+  <nav>
+    <ul>
+      <li><a href="/about.html" class="selected">Home</a></li>
+    </ul>
+    <div class="logo">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2000px-Google_2015_logo.svg.png">
     </div>
-    <!-- Logo -->
-    <div class="center">
-      <Xanzhu class="h-6 sm:h-8" aria-label="XANZHU" />
-    </div>
-    <!-- Date -->
-    <div class="right">
-      <span class="sm:block hidden">{{ $d(new Date(), "short") }}</span>
-      <span
-        class="text-red-600 font-semibold tracking-widest sm:hidden block pb-4"
-      >{{ $t('page.mobile.menu') }}</span>
-
-      <!-- Mobile -->
-      <div class="sm:hidden block">
-        <button
-          type="button"
-          class="block text-gray-500 focus:outline-none hover:text-white pt-1"
-          aria-label="Menu"
-        >
-          <Menu
-            class="h-8"
-          />
-          <Cross
-            class="h-8"
-          />
-        </button>
-      </div>
-    </div>
-  </div>
+    <ul>
+      <li><a href="/about.html" class="selected">Home</a></li>
+    </ul>
+  </nav>
 </template>
-<script>
-import Xanzhu from '~/assets/svg/xanzhu.svg'
-import TranslateIcon from '~/assets/svg/translation.svg'
-import Cross from '~/assets/svg/cross.svg'
-import Menu from '~/assets/svg/menu.svg'
-
-export default {
-  components: {
-    Xanzhu,
-    TranslateIcon,
-    Cross,
-    Menu
-  },
-  computed: {
-    availableLocales () {
-      return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
-    }
-  }
+<style>
+nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 }
-</script>
+
+.logo img {
+  width: 200px; }
+
+nav > ul {
+    display: flex;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
+
+nav > ul > li {
+    margin: 0 1rem;
+}
+</style>
