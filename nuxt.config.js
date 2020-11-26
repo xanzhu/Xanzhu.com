55c47977cd2a122c871df0dc0152dfcde1b721a7
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-11-26 14:35:12 +1300

    Final fix * Hope this works!

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   },
   head: {
     htmlAttrs: {
-      class: 'bg-green-600 dark:bg-red-600'
+      class: 'bg-gray-200 dark:bg-black'
     },
     meta: [
       { charset: 'utf-8' },
