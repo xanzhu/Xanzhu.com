194b297c62863432e0f46ca9c3529e9f607b4fc3
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-11-30 22:51:06 +1300

    Remove comments

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,54 +19,9 @@ export default {
           name: 'description',
           content: this.$i18n.t('page.description')
         }
-        // {
-        //   hid: 'Naver',
-        //   name: 'naver-site-verification',
-        //   content: 'efe8928c52c47ffddfbd678ff1ac6fe5ca08b009'
-        // }
       ],
       ...i18nSeo,
       link: [
-        // {
-        //   rel: 'alternate',
-        //   hreflang: 'en',
-        //   href: 'https://xanzhu.com/'
-        // },
-        // {
-        //   rel: 'alternate',
-        //   hreflang: 'zh',
-        //   href: 'https://xanzhu.com/zh'
-        // },
-        // {
-        //   rel: 'alternate',
-        //   hreflang: 'ko',
-        //   href: 'https://xanzhu.com/ko'
-        // },
-        // {
-        //   rel: 'alternate',
-        //   hreflang: 'ru',
-        //   href: 'https://xanzhu.com/ru'
-        // },
-        // {
-        //   rel: 'alternate',
-        //   hreflang: 'ja',
-        //   href: 'https://xanzhu.com/ja'
-        // },
-        // {
-        //   rel: 'alternate',
-        //   hreflang: 'de',
-        //   href: 'https://xanzhu.com/de'
-        // },
-        // {
-        //   rel: 'alternate',
-        //   hreflang: 'fr',
-        //   href: 'https://xanzhu.com/fr'
-        // },
-        // {
-        //   rel: 'alternate',
-        //   hreflang: 'x-default',
-        //   href: 'https://xanzhu.com/'
-        // },
         ...i18nSeo.link
       ]
     }
