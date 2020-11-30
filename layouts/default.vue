f6c379c1294580cabcc06063c41ad974438c6eb6
xanzhu <34955679+xanzhu@users.noreply.github.com>
2020-11-30 22:29:53 +1300

    Added absolute hreflang

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,12 +12,12 @@ export default {
       htmlAttrs: {
         ...i18nSeo.htmlAttrs
       },
-      title: this.$t('page.title'),
+      title: this.$i18n.t('page.title'),
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: this.$t('page.description')
+          content: this.$i18n.t('page.description')
         }
         // {
         //   hid: 'Naver',
@@ -25,32 +25,48 @@ export default {
         //   content: 'efe8928c52c47ffddfbd678ff1ac6fe5ca08b009'
         // }
       ],
+      ...i18nSeo,
       link: [
-        // {
-        //   rel: 'alternate',
-        //   hreflang: 'zh',
-        //   href: 'https://xanzhu.com/zh/'
-        // },
-        // {
-        //   rel: 'alternate',
-        //   hreflang: 'ru',
-        //   href: 'https://xanzhu.com/ru/'
-        // },
-        // {
-        //   rel: 'alternate',
-        //   hreflang: 'ja',
-        //   href: 'https://xanzhu.com/ja/'
-        // },
-        // {
-        //   rel: 'alternate',
-        //   hreflang: 'ko',
-        //   href: 'https://xanzhu.com/ko/'
-        // },
-        // {
-        //   rel: 'alternate',
-        //   hreflang: 'x-default',
-        //   href: 'https://xanzhu.com/'
-        // },
+        {
+          rel: 'alternate',
+          hreflang: 'en',
+          href: 'https://xanzhu.com/'
+        },
+        {
+          rel: 'alternate',
+          hreflang: 'zh',
+          href: 'https://xanzhu.com/zh'
+        },
+        {
+          rel: 'alternate',
+          hreflang: 'ko',
+          href: 'https://xanzhu.com/ko'
+        },
+        {
+          rel: 'alternate',
+          hreflang: 'ru',
+          href: 'https://xanzhu.com/ru'
+        },
+        {
+          rel: 'alternate',
+          hreflang: 'ja',
+          href: 'https://xanzhu.com/ja'
+        },
+        {
+          rel: 'alternate',
+          hreflang: 'de',
+          href: 'https://xanzhu.com/de'
+        },
+        {
+          rel: 'alternate',
+          hreflang: 'fr',
+          href: 'https://xanzhu.com/fr'
+        },
+        {
+          rel: 'alternate',
+          hreflang: 'x-default',
+          href: 'https://xanzhu.com/'
+        },
         ...i18nSeo.link
       ]
     }
