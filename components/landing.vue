d49c06c655ec93ad5493faf257d5b37fc1f12963
xanzhu <34955679+xanzhu@users.noreply.github.com>
2021-02-28 22:11:32 +1300

    Started basic UI design

---
+++ components/landing.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text-white text-center pb-5">
+    <h1 class="text-5xl leading-tight font-sans font-medium mt-12 mb-1">
+      Security, Technology and <br> everything in between.
+    </h1>
+    <p class="text-white text-sm">
+      Xanzhu is a platform used to share information on privacy and security.
+    </p>
+  </div>
+</template>
---
+++ components/search.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-white">
+    Search
+  </div>
+</template>
