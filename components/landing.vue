a5bf89f7029b0eb4c5527b2e1cf9d0c251a5af6c
xanzhu <34955679+xanzhu@users.noreply.github.com>
2021-03-16 21:02:14 +1300

    Cleaned up layout

--- components/landing.vue
+++ components/landing.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="dark:bg-black dark:text-white text-black bg-gray-200 text-center h-screen">
-    <h1 class="text-5xl leading-tight font-sans font-medium mx-auto pt-8 justify-center">
+    <h1 class="text-5xl leading-tight font-sans font-medium mx-auto py-4 justify-center">
       Security, Technology and <br> everything in between.
     </h1>
+    <p class="bg-blue-600">
+      Website expected to release very soon.
+    </p>
   </div>
 </template>
