eedf4a4700d7aacf583772e63e3d3604bffbb4a2
Xander <34955679+xanzhu@users.noreply.github.com>
2021-03-25 13:39:21 +1300

    Simple Privacy Policy

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-white mx-10">
+    <h1 class="text-white text-2xl">
+      Privacy Policy
+    </h1>
+    <p>
+      > No data is collected or stored when using our services.<br>
+      > Non specific usage data:<br>
+      IP Addresses, Device information, Browser information (Versions), Accessed URLS, Referring URLS.<br>
+    </p>
+  </div>
+</template>
