81da70ff146603175db1dafce1e3830f5fd2e5e1
xanzhu <34955679+xanzhu@users.noreply.github.com>
2021-04-03 22:27:53 +1300

    accessibility improvements

--- components/maintenance.vue
+++ components/maintenance.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-black items-center flex bg-gray-200 dark:bg-black dark:text-white mt-24 p-8">
-    <img class="hidden sm:block h-40 w-auto mr-12 rounded-full" src="~assets/louie.webp">
+    <img class="hidden sm:block h-40 w-auto mr-12 rounded-full" src="~assets/louie.webp" alt="Woman looking into camera wearing orange/red makeup">
     <div class="text-4xl font-medium">
       {{ $t('maintenance.title') }}
       <p class="text-base font-thin">
