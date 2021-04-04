9edc208cc1320dcee2f3afba1fa52c195771d947
xanzhu <34955679+xanzhu@users.noreply.github.com>
2021-04-04 14:35:56 +1200

    Added Image for Link Metadata

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,6 +30,12 @@ export default {
           content: this.$i18n.t('meta.description')
         },
         {
+          // Match this to page image in future
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content: 'https://res.cloudinary.com/xanzhu/image/upload/v1617503527/Xanzhu4521a_mk9agy.png'
+        },
+        {
           hid: 'og:title',
           property: 'og:title',
           content: this.$i18n.t('meta.title')
@@ -38,7 +44,13 @@ export default {
           hid: 'og:description',
           property: 'og:description',
           content: this.$i18n.t('meta.description')
-        }
+        },
+        {
+          // Match this to page image in future
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://res.cloudinary.com/xanzhu/image/upload/v1617503527/Xanzhu4521a_mk9agy.png'
+        },
       ],
       link: [
         // { rel: 'shortcut icon', sizes: '16x16 24x24 32x32 48x48 64x64', href: '/favicon.ico' },
