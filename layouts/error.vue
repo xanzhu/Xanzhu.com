d57ae0cb47e00cca3ec643ef7ebcc1bf7148328a
xanzhu <34955679+xanzhu@users.noreply.github.com>
2021-06-07 11:07:51 +1200

    House Keeping

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,7 +3,7 @@
     <h1 v-if="error.statusCode === 404" class="font-semibold text-2xl pt-32 pb-4">
       {{ $t('error.404') }}
     </h1>
-    <h1 v-else>
+    <h1 v-else class="pb-4">
       {{ $t('error.else') }}
     </h1>
     <nuxt-link :to="localePath('/')" class="focus:outline-none text-white text-sm py-1.5 px-4 rounded-md bg-gray-900 hover:shadow-lg dark:bg-red-700">
