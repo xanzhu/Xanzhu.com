578b195c8a966b15070505859448f91ec52828ec
xanzhu <34955679+xanzhu@users.noreply.github.com>
2021-06-27 21:24:59 +1200

    accessiblity tweaks * Added logic to close menu via enter/tab

--- components/header-2.vue
+++ components/header-2.vue
@@ -1,33 +1,35 @@
 <template>
   <header class="flex items-center py-3 px-6 dark:bg-black bg-gray-200 dark:text-white text-black">
-    <nav class="flex flex-1 space-x-6 justify-start ml-auto">
+    <nav class="flex flex-1 justify-start ml-auto">
       <button
-        class="sm:flex hidden"
+        class="sm:flex hidden mr-6 hover:bg-gray-300 dark:hover:bg-gray-800 rounded-md"
         aria-label="Open Language Selection"
         @click="toggle"
       >
         <Translate />
       </button>
-      <div v-if="isOpen" class="absolute bg-gray-900 text-white rounded-lg my-6">
+      <div v-if="isOpen" class="absolute z-10 dark:bg-gray-900 bg-gray-300 rounded-md mt-7 ml-1 dark:text-gray-200 font-medium flex hover:bg-gray-400 text-gray-900 dark:hover:bg-gray-700" @keydown.enter="toggle" @keydown.tab="toggle">
         <nuxt-link
           v-for="locale in availableLocales"
           :key="locale.code"
           :to="switchLocalePath(locale.code)"
         >
-          <div class="px-6" @click="isOpen = false">
+          <div class="px-6 rounded-md" :aria-label="`${locale.name}`" @click="isOpen = false">
             {{ locale.name }}
           </div>
         </nuxt-link>
       </div>
-      <nuxt-link :to="localePath('/')">
-        Home
-      </nuxt-link>
-      <nuxt-link :to="localePath('/')">
-        Downloads
-      </nuxt-link>
-      <nuxt-link :to="localePath('/')">
-        Support
-      </nuxt-link>
+      <div class="space-x-6">
+        <nuxt-link :to="localePath('/')">
+          Home
+        </nuxt-link>
+        <nuxt-link :to="localePath('/')">
+          Downloads
+        </nuxt-link>
+        <nuxt-link :to="localePath('/')">
+          Support
+        </nuxt-link>
+      </div>
     </nav>
     <nuxt-link :to="localePath('/')">
       <span class="sr-only">Xanzhu</span>
