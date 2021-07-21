bf3b6c6365d249981007746e3d8136c3640fef0d
xanzhu <34955679+xanzhu@users.noreply.github.com>
2021-07-21 17:34:00 +1200

    Improved image rendering

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="container mx-auto rounded-md dark:(bg-dark-800 text-light-200) text-dark-900 bg-light-600 mt-2 flex-grow">
     <article v-if="post">
-      <header class="dark:bg-dark-700 bg-light-700 mb-auto rounded-md flex flex-row">
-        <div class="flex flex-col flex-1 p-4">
+      <header class="dark:bg-dark-700 bg-light-700 mb-auto rounded-md flex sm:flex-row flex-col">
+        <div class="flex flex-col flex-1 px-4 py-4">
           <div class="text-xs space-x-5 inline-flex font-medium">
             <p class="text-red-500">
               {{ post.tag }}
             </p>
             <p>{{ $d(new Date(post.createdAt), 'short', localePath ) }}</p>
           </div>
-          <div class="text-2xl font-sans font-semibold mr-6 mb-auto">
+          <div class="text-lg leading-tight sm:(text-xl) md:(text-2xl leading-normal)font-sans font-semibold mr-6 mb-auto">
             {{ post.title }}
           </div>
           <div class="text-xs space-x-5 inline-flex dark:bg-dark-400 bg-light-900 p-1 rounded-full w-auto mr-auto font-medium">
@@ -17,8 +17,16 @@
           </div>
           <!-- Add article read time -->
         </div>
-        <div class="flex flex-1 justify-end items-center h-36 ml-auto overflow-hidden rounded-md">
-          <img class="h-auto w-auto" :src="post.media">
+        <div class="flex sm:(flex-1 rounded-md justify-end) justify-center items-center lg:h-36 ml-auto overflow-hidden">
+          <nuxt-img
+            class="object-contain sm:object-cover flex-strink-0 min-h-full"
+            height="200px"
+            width="100%"
+            sizes="sm:100vw md:50vw"
+            :src="post.media"
+            format="webp"
+          />
+          <!-- Add Photo credit -->
         </div>
       </header>
       <nuxt-content class="mx-6 my-4 dark:text-light-200 text-dark-800" :document="post" />
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="blog container mx-8 dark:text-light-200 text-dark-900 flex-grow">
-    <section class="grid grid-cols-3 gap-2 pt-12">
+  <div class="blog container sm:mx-6 dark:text-light-200 text-dark-900 flex-grow">
+    <section class="grid grid-cols-1 md:grid-cols-3 gap-2 pt-12 justify-center">
       <article
         v-for="(post, $index) in posts"
         :key="`post-${$index}`"
-        class="post max-w-sm rounded overflow-hidden shadow-lg flex flex-col dark:bg-dark-900 bg-light-600"
+        class="post max-w-sm rounded overflow-hidden shadow-lg flex flex-col mx-auto dark:bg-dark-900 bg-light-600"
       >
         <nuxt-link :to="localePath(post.path)" class="font-bold text-xl mb-2">
-          <img class="relative w-full h-32 object-cover object-center" :src="post.media" :alt="post.title">
+          <nuxt-img class="relative w-full h-32 object-cover object-center" :src="post.media" :alt="post.title" format="webp" sizes="sm:100vw md:50vw" />
           <div class="py-2 px-2 flex-2">
             <div class="flex flex-row">
               <p class="text-red-500 font-medium text-sm flex-1">
