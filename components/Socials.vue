3958fa33662b445dcae37955e4410c9e3bd84ca1
xanzhu <34955679+xanzhu@users.noreply.github.com>
2021-08-08 16:27:30 +1200

    Added socials

---
+++ components/Socials.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Social share wrapper text-red-600 flex-row flex items-center space-x-4">
+    <a href="https://twitter.com/Xanzhu1" rel="noopener" target="_blank" class="hover:text-dark-900 dark:(hover:text-light-200)">
+      <Twitter class="h-5 w-5" />
+    </a>
+    <a href="https://www.linkedin.com/company/xanzhu" rel="noopener" target="_blank" class="hover:text-dark-900 dark:(hover:text-light-200)">
+      <LinkedIn class="h-5 w-5" />
+    </a>
+    <a href="https://github.com/xanzhu" rel="noopener" target="_blank" class="hover:text-dark-900 dark:(hover:text-light-200)">
+      <Github class="h-5 w-5" />
+    </a>
+  </div>
+</template>
+<script>
+import Twitter from '~/assets/icons/twitter.svg?inline'
+import LinkedIn from '~/assets/icons/linkedin.svg?inline'
+import Github from '~/assets/icons/github.svg?inline'
+
+export default {
+  components: {
+    Twitter,
+    LinkedIn,
+    Github
+  }
+}
+</script>
--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="flex items-center sm:(h-6 flex-row space-y-0 py-8) dark:(bg-black text-light-200) bg-gray-200 w-full text-dark-900 px-6 flex-col space-y-2 py-4 text-xs">
+  <footer class="flex items-center sm:(h-6 flex-row space-y-0 py-8) dark:(bg-black text-light-200) bg-gray-200 w-full text-dark-900 flex-col space-y-2 py-4 px-4 md:px-6 lg:px-12 xl:px-32 text-xs">
     <div class="flex flex-1 justify-start space-x-6 py-1">
       <nuxt-link :to="localePath('/privacy-policy')" class="border-b-2 hover:border-red-600 border-transparent">
         {{ $t('links.privacy_policy') }}
@@ -7,11 +7,12 @@
       <a href="https://status.xanzhu.com" rel="nofollow noopener" target="_blank" class="hover:(bg-red-600 border-transparent) border-b-2 border-red-600">
         {{ $t('links.status') }}
       </a>
+      <Socials />
     </div>
     <div class="hidden sm:flex w-5 h-5">
       <Xanzhuico />
     </div>
-    <div class="flex flex-1 justify-end">
+    <div class="flex flex-1 justify-end items-center">
       © XANZHU 2021 (Developer v0.3.2)
     </div>
   </footer>
