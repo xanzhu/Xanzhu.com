c84d5de13a46b8281db8cae3c349c41d92b2cbe9
xanzhu <34955679+xanzhu@users.noreply.github.com>
2021-08-09 19:13:12 +1200

    Accessibility Improvements - Testing direct names in svg

--- components/Socials.vue
+++ components/Socials.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="Social share wrapper text-red-600 flex-row flex items-center space-x-4">
-    <a href="https://twitter.com/Xanzhu1" rel="noopener" target="_blank" class="hover:text-dark-900 dark:(hover:text-light-200)">
-      <Twitter class="h-5 w-5" aria-label="Twitter" />
+  <div class="text-red-600 flex-row flex items-center space-x-4">
+    <a href="https://twitter.com/Xanzhu1" rel="noopener" target="_blank" class="hover:text-dark-900 dark:(hover:text-light-200)" aria-label="Twitter">
+      <Twitter class="h-5 w-5" />
     </a>
-    <a href="https://www.linkedin.com/company/xanzhu" rel="noopener" target="_blank" class="hover:text-dark-900 dark:(hover:text-light-200)">
-      <LinkedIn class="h-5 w-5" aria-label="LinkedIn" />
+    <a href="https://www.linkedin.com/company/xanzhu" rel="noopener" target="_blank" class="hover:text-dark-900 dark:(hover:text-light-200)" aria-label="LinkedIn">
+      <LinkedIn class="h-5 w-5" />
     </a>
-    <a href="https://github.com/xanzhu" rel="noopener" target="_blank" class="hover:text-dark-900 dark:(hover:text-light-200)">
-      <Github class="h-5 w-5" aria-label="Github" />
+    <a href="https://github.com/xanzhu" rel="noopener" target="_blank" class="hover:text-dark-900 dark:(hover:text-light-200)" aria-label="Github">
+      <Github class="h-5 w-5" />
     </a>
   </div>
 </template>
