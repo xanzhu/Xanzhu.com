df1fc42c457f271bba03363a5a13cde9b3954ed0
Xander Dagani <34955679+xanzhu@users.noreply.github.com>
2021-08-19 20:59:54 +1200

    feat: Added support for Vite (#155)
    - Dropped support for Webpack
    - Dropped support for svg loader (May look into this again)

---
+++ components/Icon/Cross.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg class="stroke-current stroke-cap-round stroke-join-round stroke-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 18L18 6M6 6l12 12" />
+  </svg>
+</template>
