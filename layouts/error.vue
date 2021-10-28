d2f09b80fa4d1defb89277171c33ad141cc7ec81
xanzhu <34955679+xanzhu@users.noreply.github.com>
2021-10-28 13:15:59 +1300

    Fix 404 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -4,7 +4,7 @@
       <h1 v-if="error.statusCode === 404" v-t="'error.404'" />
       <h1 v-else v-t="'error.else'" />
     </div>
-    <NuxtLink v-t="'error.return'" v:to="localePath('/blog')" class="focus:outline-none text-sm py-1.5 text-light-200 font-semibold px-4 rounded-md bg-dark-900 hover:shadow-lg dark:bg-red-700" />
+    <NuxtLink v-t="'error.return'" :to="localePath('/blog')" class="focus:outline-none text-sm py-1.5 text-light-200 font-semibold px-4 rounded-md bg-dark-900 hover:shadow-lg dark:bg-red-700" />
   </div>
 </template>
 <script>
