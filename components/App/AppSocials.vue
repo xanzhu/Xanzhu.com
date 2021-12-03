a8921ccd7a76ac13f1d1e42d7ba44d76e918ae6d
xanzhu <34955679+xanzhu@users.noreply.github.com>
2021-12-03 21:42:59 +1300

    Added medium icon - Added new social media icon for medium

--- components/App/AppSocials.vue
+++ components/App/AppSocials.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-row text-red-600 space-x-4">
+  <div class="flex flex-row text-red-600 space-x-4 items-center">
     <a href="https://twitter.com/Xanzhu1" rel="noopener noreferrer" target="_blank" class="hover:text-dark-900 dark:(hover:text-light-300)" aria-label="Twitter">
       <Icon-Twitter class="h-5 w-5" />
     </a>
@@ -8,6 +8,9 @@
     </a>
     <a href="https://github.com/xanzhu" rel="noopener noreferrer" target="_blank" class="hover:text-dark-900 dark:(hover:text-light-300)" aria-label="Github">
       <Icon-Github class="h-5 w-5" />
+    </a>
+    <a href="https://medium.com/@xanzhu" rel="noopener noreferrer" target="_blank" class="hover:text-dark-900 dark:(hover:text-light-300)" aria-label="Medium">
+      <Icon-Medium />
     </a>
   </div>
 </template>
