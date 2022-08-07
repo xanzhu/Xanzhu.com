bcdab620bd5acc8e113a07189f2c012d13d9c2ca
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-08-07 20:34:41 +1200

    Minor tweaks

--- components/App/AppFooter.vue
+++ components/App/AppFooter.vue
@@ -1,11 +1,11 @@
 <template>
   <footer
-    class="flex flex-col-reverse p-1 sm:(flex-row px-2) justify-between text-black bg-light-400 dark:(text-white bg-dark-900) items-center">
+    class="flex flex-col-reverse p-1 sm:(flex-row px-2) justify-between text-black sm:bg-light-400 dark:(text-white sm:bg-dark-900) items-center">
     <div class="flex sm:(flex-col p-2)">
       <p class="dark:text-[#FF0000] text-red-700 font-medium text-xs hidden sm:flex" v-t="'footer.rights'" />
       <p class="text-xs" v-t="'footer.copy'" />
     </div>
-    <AppSocials />
+    <AppSocials class="sm:mt-2" />
     <div class="text-xs mx-4">
       <div class="space-x-3 p-1 sm:p-0">
         <NuxtLink :to="localePath('/privacy-policy')" class="nav-link">
@@ -16,7 +16,7 @@
           {{ $t("links.status") }}
           <IconExternal class="h-3 flex-col inline-flex -mt-0.5" />
         </a>
-        <p class="justify-end hidden sm:flex">{{ $t('footer.dev') }} v0.3.5</p>
+        <p class="justify-end hidden font-thin sm:flex">{{ $t('footer.dev') }} v0.3.5</p>
       </div>
     </div>
   </footer>
