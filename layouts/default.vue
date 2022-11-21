6410778003c5cbad7445f9d9b6e3e6d99a3fcde3
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-11-21 13:09:33 +1300

    Add title prefix to og values

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,21 +6,21 @@ const head = useLocaleHead({
   identifierAttribute: 'id',
   addSeoAttributes: true
 })
+// Update title meta translations to reflect "Pagename | Xanzhu"
 const title = computed(() => t(route.meta.title), { title: t(route.meta.title) })
 
 useHead({
   meta: [
-    { property: 'og:title', content: t(route.meta.title) },
+    { property: 'og:title', content: t(route.meta.title) + ' | Xanzhu' },
     { property: 'description', content: t(route.meta.description) },
     { property: 'og:description', content: t(route.meta.description) },
-    { property: 'twitter:title', content: t(route.meta.title) },
+    { property: 'twitter:title', content: t(route.meta.title) + '| Xanzhu' },
     { property: 'twitter:description', content: t(route.meta.description) },
     // { property: 'og:url', content: 'https://xanzhu.com' } Add later
   ]
 })
 
 </script>
-
 <template>
   <div>
     <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
@@ -42,67 +42,9 @@ useHead({
     </Html>
   </div>
 </template>
-
-
-<!-- <template>
-  <div class="flex flex-col min-h-screen subpixel-antialiased">
-    <AppHeader />
-    <slot />
-    <AppFooter />
-  </div>
-</template>
-<script>
-export default {
-  layout: 'AppDefault',
-  head () {
-    const i18nHead = this.localeHead({ addDirAttribute: true, addSeoAttributes: true })
-    return {
-      htmlAttrs: {
-        ...i18nHead.htmlAttrs
-      },
-      title: this.$i18n.t('meta.title'),
-      meta: [
-        {
-          hid: 'description',
-          name: 'description',
-          content: this.$i18n.t('meta.description')
-        },
-        {
-          hid: 'twitter:image',
-          name: 'twitter:image',
-          content: 'https://res.cloudinary.com/xanzhu/image/upload/v1627084292/Twitter-image-v1.2_b0ebgw.png'
-        },
-        {
-          hid: 'og:title',
-          property: 'og:title',
-          content: this.$i18n.t('meta.title')
-        },
-        {
-          hid: 'og:description',
-          property: 'og:description',
-          content: this.$i18n.t('meta.description')
-        },
-        {
-          hid: 'og:image',
-          property: 'og:image',
-          content: 'https://res.cloudinary.com/xanzhu/image/upload/v1627084292/Twitter-image-v1.2_b0ebgw.png'
-        },
-        {
-          hid: 'og:url',
-          property: 'og:url',
-          content: 'https://a2.xanzhu.com' + this.$nuxt.$route.path
-        }
-      ],
-      link: [
-        ...i18nHead.link
-      ]
-    }
-  }
-}
-</script>
 <style>
 svg {
   display: inline-block;
   vertical-align: inherit;
 }
-</style> -->
+</style>
