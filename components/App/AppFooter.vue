2241a5c2fe9d8f157db198ec7b84523622d0c7d5
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-12-01 22:25:49 +1300

    Minor tweaks

--- components/App/AppFooter.vue
+++ components/App/AppFooter.vue
@@ -1,19 +1,19 @@
-<script setup>
+<script setup lang="ts">
 const { t } = useI18n()
+const localePath = useLocalePath()
 </script>
-
 <template>
   <footer
     class="flex flex-col-reverse p-1 sm:(flex-row px-2) justify-between text-black sm:bg-light-400 dark:(text-white sm:bg-dark-900) items-center">
     <div class="flex sm:(flex-col p-2)">
-      <p class="dark:text-[#FF0000] text-red-700 font-medium text-xs hidden sm:flex">{{t('footer.rights')}}</p>
-      <p class="text-xs">{{ t('footer.copy')}}</p>
+      <p class="dark:text-[#FF0000] text-red-700 font-medium text-xs hidden sm:flex">{{ t('footer.rights') }}</p>
+      <p class="text-xs">{{ t('footer.copy') }}</p>
     </div>
     <AppSocials class="sm:mt-2" />
     <div class="text-xs mx-4">
       <div class="space-x-3 p-1 sm:p-0">
         <NuxtLink :to="localePath('/privacy-policy')" class="nav-link">
-          {{ t("links.privacy_policy")}}
+          {{ t("links.privacy_policy") }}
         </NuxtLink>
         <NuxtLink to="https://status.xanzhu.com" rel="noopener noreferrer" target="_blank" class="nav-link"
           :aria-label="t('links.status') + ' ' + t('aria.button.NewWindow')">
