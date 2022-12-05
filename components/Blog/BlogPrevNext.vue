158b92fae223d80936967b450bf4e5ac32b4fc80
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-12-05 14:02:31 +1300

    Minor tweak

--- components/Blog/BlogPrevNext.vue
+++ components/Blog/BlogPrevNext.vue
@@ -11,21 +11,23 @@
         class="border-1 border-dark-50 rounded-lg px-3.5 py-1.4 text-sm m-4 dark:text-[#FF0000] dark:hover:(bg-red-600 text-black border-transparent) hover:(bg-red-600 text-white border-transparent) text-red-600 md:(text-xl font-medium)">
         <p v-t="'article.Next'" />
       </NuxtLink>
+      <span v-else>&nbsp;</span>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 export interface PrevNext {
-  title?: string;
+  title?: string
   _path?: string
 }
 
 defineProps<{
-  prev: PrevNext
-  next: PrevNext
+  prev?: PrevNext
+  next?: PrevNext
 }>()
 </script>
 <!-- TO:DO
 - Fix sizing
+- bug not working?? test in vercel
 -->
