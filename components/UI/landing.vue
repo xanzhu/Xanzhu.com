72fbd06044a296aeed0adee89309b4ee652c3b2e
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-12-10 21:04:48 +1300

    Improved Styles - Mobile support - Native Translations

--- components/UI/landing.vue
+++ components/UI/landing.vue
@@ -3,19 +3,14 @@ const { t } = useI18n()
 </script>
 <template>
     <div
-        class="flex sm:(p-15 space-y-0 items-center) md:flex-row flex-col-reverse dark:(text-white bg-black) p-7 items-center">
+        class="flex sm:(p-15 space-y-0 items-center) md:(flex-row justify-center) flex-col-reverse dark:(text-white bg-black) p-4 items-center">
         <div class="flex flex-col justify-center md:space-y-6 sm:mx-10 space-y-4">
             <!-- Update locale naming scheme -->
             <h1 class="text-3xl lg:text-4xl font-semibold">{{ t('blog.landing.desc') }}</h1>
             <!-- Add Translation for this and improve wording-->
-            <p class="text-sm sm:text-lg text-gray-700 dark:(text-gray-300) md:pr-25 font-thin">Xanzhu is a platform
-                used to share
-                knowledge.
-                Helping build the next standard of privacy and accessibility. Teaching how to use technology and remain
-                safe
-                online.</p>
+            <p class="text-sm sm:text-lg text-gray-700 dark:(text-gray-300) md:pr-25 font-thin">{{ t('home.landing-desc')}}</p>
         </div>
-        <div class="pb-10 md:pb-0 flex-0 md:flex-grow">
+        <div class="pb-10 md:pb-0 flex-0 max-w-md">
             <!-- Add sizing -->
             <NuxtImg class="dark:(border-dark-300 border-1) rounded-md"
                 src="https://source.unsplash.com/K5u5bXMGIwM/800x1000" />
