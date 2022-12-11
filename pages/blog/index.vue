8e41dc67731ae5e931fdb8b4ffadcf195e2bf5fe
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-12-11 23:56:54 +1300

    Design updated - Improved Blog landing - Added translations

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -13,33 +13,36 @@ definePageMeta({
 
 </script>
 <template>
-  <div class="max-w-sm mx-auto my-4 sm:(container mx-auto mb-10) dark:text-light-200 text-black flex-grow" role="main">
-    <h1 class="text-center text-red-600 font-semibold text-4xl md:text-6xl mt-">
-      {{ t('blog.landing.title') }}
-    </h1>
-    <p class="text-lg p-2 mb-5 text-center break-words sm:(text-xl) md:(mb-10) dark:text-gray-300">
-      {{ t('blog.landing.desc') }}
-    </p>
-    <section class="grid grid-cols-1 gap-4 sm:(grid-cols-2 mx-8) md:(grid-cols-2 gap-6) lg:(grid-cols-3 mx-10)">
-      <article v-for="(article, $index) in articles" :key="`post-${$index}`"
-        class="max-w-lg w-full rounded overflow-hidden flex flex-col shadow-md dark:(bg-dark-900 shadow-none border-dark-700 border-1) bg-white mx-auto border-0"
-        role="article">
-        <NuxtLink :to="(article._path)" class="font-bold text-xl mb-2">
-          <NuxtImg crossorigin="anonymous" v-if="article.media"
-            class="relative w-full h-32 object-cover sm:(object-center h-40)" :src="article.media" :alt="article.alt"
-            format="webp" height="290" width="130" sizes="sm:100vw md:50vw lg:25vw" loading="eager" fit="cover"
-            provider="cloudinary" />
-          <div class="flex flex-row items-center px-4 py-2 space-x-2">
-            <p class="text-red-500 bg-transparent rounded-md text-xs dark:(text-[#FF0000])">
+  <div class="mx-auto space-y-20 dark:text-white text-black sm:mb-10" role="main">
+    <div class="mt-15 mx-2 sm:mx-8 lg:mx-10">
+      <h1 class="font-semibold text-4xl">
+        {{ t('blog.landing.title') }}
+      </h1>
+      <p class="text-lg break-words font-thin sm:(text-xl) dark:text-gray-300">
+        {{ t('blog.landing.desc') }}
+      </p>
+    </div>
+    <section class="grid grid-cols-1 gap-10 md:(grid-cols-2 mx-6) lg:(grid-cols-3) p-4 sm:(py-15 px-10) dark:(bg-dark-900 border-dark-300) border-1 border-gray-200 bg-gray-100 rounded">
+      <div v-if="articles" v-for="(article, $index) in articles" :key="`fe-${$index}`">
+        <NuxtLink class="flex flex-col space-y-3" :to="(article._path)">
+          <NuxtImg v-if="article.img" class="dark:(border-dark-300 border-1) rounded-md max-h-xl" :src="article.img" />
+          <NuxtImg v-else-if="article.media" :src="article.media" sizes="sm:100vw md:50vw lg:25vw" provider="cloudinary"
+            class="dark:(border-dark-300 border-1) rounded-md max-h-xl" />
+          <NuxtImg v-else class="dark:(border-dark-300 border-1) rounded-md"
+            src="https://source.unsplash.com/kUmcSBJcFPg/577x369" />
+          <div class="flex flex-row space-x-2">
+            <p v-if="article.tag"
+              class="dark:(bg-black text-white border-dark-300) px-4 border-1 py-1.5 rounded-md text-xs">
               {{ article.tag }}
             </p>
-            <Date v-if="article.date" :date="article.date" class="text-sm font-medium text-right" />
+            <Date v-if="article.date" :date="article.date"
+              class="dark:(bg-black text-white border-dark-300) mr-auto px-4 border-1 py-1.5 rounded-md text-xs" />
           </div>
-          <h2 class="rounded-md font-medium text-xl px-4">
+          <h2 class="rounded-md font-medium text-xl dark:(text-white)">
             {{ article.title }}
           </h2>
         </NuxtLink>
-      </article>
+      </div>
     </section>
   </div>
 </template>
