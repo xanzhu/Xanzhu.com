6508cca21fc1514910a2c2943550b662459f7120
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-12-11 20:39:45 +1300

    Optimised SVGs

--- components/Icon/IconLinkedIn.vue
+++ components/Icon/IconLinkedIn.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg viewBox="0 0 24 24" class="fill-current h-5 w-5 hover:text-[#0072b1]" xmlns="http://www.w3.org/2000/svg"
+  <svg viewBox="0 0 24 24" fill="currentColor" class="h-5 w-5 hover:text-[#0072b1]" xmlns="http://www.w3.org/2000/svg"
     aria-labelledby="ico-linkedin">
     <title id="ico-linkedin">LinkedIn</title>
     <path
--- components/Icon/IconMenu.vue
+++ components/Icon/IconMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg viewBox="0 0 24 24" class="fill-current" xmlns="http://www.w3.org/2000/svg">
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
     <path d="M21 16H3v-2h18v2zm0-6H3V8h18v2z" />
   </svg>
 </template>
