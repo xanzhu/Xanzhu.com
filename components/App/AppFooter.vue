82a1e259c868bd0a38c2999741a981963aef0443
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-12-11 20:33:02 +1300

    Footer fixed

--- components/App/AppFooter.vue
+++ components/App/AppFooter.vue
@@ -6,17 +6,26 @@ const localePath = useLocalePath()
   <footer
     class="flex flex-col-reverse p-1 sm:(flex-row px-2) justify-between text-black sm:bg-light-400 dark:(text-white sm:bg-dark-900) items-center">
     <div class="flex sm:(flex-col p-2)">
-      <p class="dark:text-[#FF0000] text-red-700 font-medium text-xs hidden sm:flex">{{ t('footer.rights') }}</p>
-      <p class="text-xs">{{ t('footer.copy') }}</p>
+      <p class="text-xs font-medium">© XANZHU 2022</p>
+      <p class="font-thin text-xs hidden sm:flex">{{ t('footer.rights') }}</p>
     </div>
-    <AppSocials class="sm:mt-2" />
+    <div class="flex-row flex children:p-1">
+      <NuxtLink to="https://www.linkedin.com/company/xanzhu" target="_blank">
+        <IconLinkedIn />
+      </NuxtLink>
+      <NuxtLink to="https://github.com/xanzhu" target="_blank">
+        <IconGithub />
+      </NuxtLink>
+      <NuxtLink to="https://twitter.com/Xanzhu1" target="_blank">
+        <IconTwitter />
+      </NuxtLink>
+    </div>
     <div class="text-xs mx-4">
       <div class="space-x-3 p-1 sm:p-0">
         <NuxtLink :to="localePath('/privacy-policy')" class="nav-link">
           {{ t("links.privacy_policy") }}
         </NuxtLink>
-        <NuxtLink to="https://status.xanzhu.com" rel="noopener noreferrer" target="_blank" class="nav-link"
-          :aria-label="t('links.status')">
+        <NuxtLink to="https://status.xanzhu.com" target="_blank" class="nav-link">
           {{ t("links.status") }}
           <IconExternal class="h-3 flex-col inline-flex -mt-0.5" />
         </NuxtLink>
