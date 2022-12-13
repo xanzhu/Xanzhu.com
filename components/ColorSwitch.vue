a9bc80dae7cde468a984b0de9ab37563b1925453
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-12-13 17:47:35 +1300

    Minor tweaks

--- components/ColorSwitch.vue
+++ components/ColorSwitch.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 const color = useColorMode()
 const { t } = useI18n()
+
 function colorSwitch() {
   color.preference = color.value === 'dark' ? 'light' : 'dark'
 }
-</script>
 
+function ariaLabel() {
+  return color.preference === 'dark' ? t('aria.button.light') : t('aria.button.dark')
+}
+
+</script>
 <template>
-  <button @click="colorSwitch" aria-label="Theme Switch" class="text-dark-900 dark:text-light-200 px-1 pt-0.5">
-    <IconSun v-if="color.preference === 'dark'" :aria-label="t('aria.button.light')" class="h-5 w-5" />
-    <IconMoon v-else :aria-label="t('aria.button.dark')" class="h-5 w-5" />
+  <button @click="colorSwitch" :aria-label="ariaLabel" class="text-dark-900 dark:text-light-200 px-1 pt-0.5">
+    <IconSun v-if="color.preference === 'dark'" class="h-5 w-5" />
+    <IconMoon v-else class="h-5 w-5" />
   </button>
 </template>
