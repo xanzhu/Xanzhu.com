21d86347717ffff53c348f935321a2e0fee42a4a
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-12-18 23:09:23 +1300

    Minor tweak

--- components/Blog/BlogPrevNext.vue
+++ components/Blog/BlogPrevNext.vue
@@ -1,20 +1,17 @@
-
 <template>
-  <div class="flex flex-col space-y-5 pt-5 mx-5 sm:(mx-20 pt-10)">
+  <nav class="flex flex-col space-y-5 pt-5 mx-5 sm:(mx-20 pt-10)">
     <div class="flex flex-row justify-between font-normal children:(md:text-xl text-sm m-4 px-3.5 py-1.4)">
       <NuxtLink v-if="prev" :to="prev._path"
         class="border-outline dark:hover:(bg-white text-black) hover:(bg-black text-white)">
         <p v-t="'article.Previous'" />
       </NuxtLink>
-      <!-- <span v-else>&nbsp;</span> -->
       <NuxtLink v-if="next" :to="next._path"
         class="border-outline dark:text-[#FF0000] dark:hover:(bg-[#FF0000] text-black) hover:(bg-[#FF0000] text-white) text-dark-600">
         <p v-t="'article.Next'" />
       </NuxtLink>
     </div>
-  </div>
+  </nav>
 </template>
-
 <script setup lang="ts">
 export interface PrevNext {
   title?: string
@@ -26,7 +23,3 @@ defineProps<{
   next?: PrevNext
 }>()
 </script>
-<!-- TO:DO
-- Fix sizing
-- bug not working?? test in vercel
--->
