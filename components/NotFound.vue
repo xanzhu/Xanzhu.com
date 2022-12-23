69335ddeb152f7eabd543a8ba2d42b50d115c94c
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-12-23 15:31:01 +1300

    Minor Tweaks

--- components/NotFound.vue
+++ components/NotFound.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
 const { t } = useI18n();
 const localePath = useLocalePath()
-
 </script>
 <template>
-    <div class="bg-black flex flex-col text-white justify-center items-center space-y-25">
+    <div class="h-full bg-black flex flex-col text-white justify-center items-center space-y-25">
         <div class="flex flex-col items-center sm:(flex-row space-x-10 text-xl)">
             <NuxtImg src="/images/404.png" width="270" height="151" :alt="t('error.404')" />
-            <h1>{{ t('error.404') }}</h1>
+            <h1 class="text-center text-sm">{{ t('error.404') }}</h1>
         </div>
         <NuxtLink v-t="'error.return'" :to="localePath('/')"
-            class="border-2 border-white py-1 px-8 hover:(bg-white text-black)" />
+            class="border-outline py-2 px-8 hover:(bg-white text-black) leading-none" />
     </div>
 </template>
