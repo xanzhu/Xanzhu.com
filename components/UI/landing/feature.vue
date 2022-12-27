30b42033dbfb8d409b514c79bc703364971f86cc
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-12-27 19:47:39 +1300

    Design Improvements - Updated Zoomef - Updated Date Design - Comp structure

--- components/UI/landing/feature.vue
+++ components/UI/landing/feature.vue
@@ -39,13 +39,13 @@ const { data: features } = await useAsyncData('feature',
         </div>
         <div v-if="wideFeature" v-for="wide in wideFeature" :key="wide.title" class="sm:(mx-5 p-0) lg:mx-20 p-4">
             <NuxtLink :to="(wide._path)"
-                class="text-black dark:(text-white) group flex flex-col lg:flex-row lg:items-center mx-auto space-y-3">
+                class="text-black dark:(text-white) group zoomef flex flex-col lg:flex-row lg:items-center mx-auto space-y-3">
                 <NuxtImg crossorigin="anonymous" v-if="wide.img" :src="wide.img" :alt="wide.alt" loading="lazy"
-                    class="border-outline md:(max-w-lg mx-auto) xl:max-w-2xl zoomef" height="1253" width="1880" />
+                    class="border-outline md:(max-w-lg mx-auto) xl:max-w-2xl" height="1253" width="1880" />
                 <NuxtImg crossorigin="anonymous" v-else-if="wide.media" :src="wide.media" :alt="wide.alt" loading="lazy"
-                    provider="cloudinary" class="border-outline md:max-w-2xl zoomef" height="1253" width="1880" />
+                    provider="cloudinary" class="border-outline md:max-w-2xl" height="1253" width="1880" />
                 <NuxtImg crossorigin="anonymous" v-else :alt="wide.alt" loading="lazy"
-                    src="https://source.unsplash.com/bHOKatJHjII/577x369" class="border-outline md:max-w-2xl zoomef"
+                    src="https://source.unsplash.com/bHOKatJHjII/577x369" class="border-outline md:max-w-2xl"
                     height="1253" width="1880" />
                 <div class="sm:(space-y-7) md:space-y-2 space-y-4 lg:p-6">
                     <div class="flex flex-row space-x-2">
@@ -66,24 +66,22 @@ const { data: features } = await useAsyncData('feature',
         </div>
         <section class="grid grid-cols-1 gap-10 md:(grid-cols-2) p-4 sm:p-5 lg:p-20">
             <div v-if="features" v-for="(feature, $index) in features" :key="`fe-${$index}`">
-                <NuxtLink v-if="feature.feature" class="flex flex-col space-y-3 group" :to="(feature._path)">
+                <NuxtLink v-if="feature.feature" class="flex flex-col group relative zoomef" :to="(feature._path)">
                     <NuxtImg crossorigin="anonymous" v-if="feature.img" :alt="feature.alt" loading="lazy" height="369"
-                        width="577" class="border-outline zoomef" :src="feature.img" />
+                        width="577" class="border-outline" :src="feature.img" />
                     <NuxtImg crossorigin="anonymous" v-else-if="feature.media" :src="feature.media" :alt="feature.alt"
                         height="369" width="577" sizes="sm:100vw md:50vw lg:25vw" provider="cloudinary" loading="lazy"
-                        class="border-outline zoomef" />
+                        class="border-outline" />
                     <NuxtImg crossorigin="anonymous" :alt="feature.alt" loading="lazy" v-else height="369" width="577"
-                        class="border-outline zoomef" src="https://source.unsplash.com/kUmcSBJcFPg/577x369" />
-                    <div class="flex flex-row space-x-2">
-                        <p v-if="feature.tag"
-                            class="dark:(bg-black text-white) bg-white px-4 border-outline py-1.5 text-xs">
-                            {{ feature.tag }}
-                        </p>
-                        <Date v-if="feature.date" :date="feature.date"
-                            class="dark:(bg-black text-white) bg-white border-outline mr-auto px-4 py-1.5 text-xs" />
-                    </div>
+                        class="border-outline" src="https://source.unsplash.com/kUmcSBJcFPg/577x369" />
+                    <Date v-if="feature.date" :date="feature.date"
+                        class="dark:(bg-black text-white) bg-white border-1 px-4 py-2 border-outline rounded-tr-none rounded-bl-none rounded-tl-br-md text-xs absolute" />
+                    <p v-if="feature.tag"
+                        class="dark:(bg-black text-white) mr-auto mt-3 bg-white px-4 border-outline py-1.5 text-xs group-hover:(text-border-600)">
+                        {{ feature.tag }}
+                    </p>
                     <h2
-                        class="rounded-md font-medium text-xl dark:(text-white) sm:group-hover:(underline decoration-1 underline-offset-4)">
+                        class="rounded-md font-medium mt-2 text-xl dark:(text-white) sm:group-hover:(underline decoration-1 underline-offset-4)">
                         {{ feature.title }}
                     </h2>
                 </NuxtLink>
--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@ useHead({
 </script>
 <template>
   <div class="scroll-smooth space-y-10 mb-10">
-    <UILanding class="md:h-[42rem]" />
-    <UIFeature class="sm:mx-10" />
+    <UILandingSplash class="md:h-[42rem]" />
+    <UILandingFeature class="sm:mx-10" />
   </div>
 </template>
