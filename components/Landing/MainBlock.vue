4e1c0f428d19d244babc052ab93bae8fce7049cb
xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-01-02 16:37:30 +1300

    Added Splash Credit

--- components/Landing/MainBlock.vue
+++ components/Landing/MainBlock.vue
@@ -10,8 +10,15 @@ const { t } = useI18n()
             <h1 class="text-3xl sm:text-5xl font-semibold text-white">{{ t('blog.landing.desc') }}</h1>
         </div>
         <div>
-            <p class="absolute text-white font-thin top-1/2 ml-4 p-8 bg-black right-0 sm:(w-1/2 top-1/2)"> {{ t('home.landing-desc') }}
+            <p class="absolute text-white font-thin top-1/2 ml-4 p-8 bg-black right-0 sm:(w-1/2 top-1/2)"> {{
+        t('home.landing-desc')
+}}
             </p>
         </div>
+        <span class="absolute font-thin text-xs bottom-0 right-0 m-2">{{ t('article.creditsource') }}: <NuxtLink
+                class="font-normal" to="https://www.nasa.gov/" target="_blank">NASA</NuxtLink>, <NuxtLink
+                class="font-normal"
+                to="https://unsplash.com/photos/Q1p7bh3SHj8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                target="_blank">Unsplash</NuxtLink></span>
     </section>
 </template>
