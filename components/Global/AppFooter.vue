b5cbc77e11f74d120514e54eb5e8a3233ccc31d5
xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-01-02 18:13:37 +1300

    Updated to 2023

--- components/Global/AppFooter.vue
+++ components/Global/AppFooter.vue
@@ -2,7 +2,7 @@
   <footer
     class="flex flex-col-reverse p-1 sm:(flex-row px-2) justify-between text-black sm:bg-light-400 dark:(text-white sm:bg-dark-900) items-center">
     <div class="flex sm:(flex-col p-2)">
-      <p class="text-xs font-medium">&copy; XANZHU 2022</p>
+      <p class="text-xs font-medium">&copy; XANZHU 2023</p>
       <p class="font-thin text-xs hidden sm:flex">{{ t('footer.rights') }}</p>
     </div>
     <div class="flex-row flex children:p-1">
@@ -28,7 +28,7 @@
           {{ t("links.status") }}
           <IconExternal class="h-3 flex-col inline-flex -mt-0.5" />
         </NuxtLink>
-        <p class="justify-end hidden font-thin sm:flex">{{ t('footer.build') }} v0.3.7</p>
+        <p class="justify-end hidden font-thin sm:flex">{{ t('footer.build') }} v0.3.8</p>
       </div>
     </div>
   </footer>
