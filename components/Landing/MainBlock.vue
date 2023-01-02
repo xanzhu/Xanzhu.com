f7970b5e39b38198e24bf75e9b83507d6ab0f2cf
xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-01-02 21:14:31 +1300

    Improved Mobile Sizing

--- components/Landing/MainBlock.vue
+++ components/Landing/MainBlock.vue
@@ -7,15 +7,15 @@ const { t } = useI18n()
             style="background-image: url('/images/landing/globe.webp');">
         </div>
         <div class="absolute justify-start p-4 sm:p-12 left-0 top-4">
-            <h1 class="text-3xl sm:text-5xl font-semibold text-white">{{ t('blog.landing.desc') }}</h1>
+            <h1 class="pt-10 text-3xl sm:(pt-0 text-5xl) font-semibold text-white">{{ t('blog.landing.desc') }}</h1>
         </div>
         <div>
-            <p class="absolute text-white font-thin top-1/2 ml-4 p-8 bg-black right-0 sm:(w-1/2 top-1/2)"> {{
+            <p class="absolute text-white font-thin top-1/2 ml-4 p-6 bg-black right-0 sm:(w-1/2 top-1/2 p-8)"> {{
         t('home.landing-desc')
 }}
             </p>
         </div>
-        <span class="absolute font-thin text-xs bottom-0 right-0 m-2 p-1">{{ t('article.creditsource') }}: <NuxtLink
+        <span class="absolute font-thin text-xs bottom-0 right-0 m-3 p-1">{{ t('article.creditsource') }}: <NuxtLink
                 class="font-normal" to="https://www.nasa.gov/" target="_blank">NASA</NuxtLink>, <NuxtLink
                 class="font-normal"
                 to="https://unsplash.com/photos/Q1p7bh3SHj8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
