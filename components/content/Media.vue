f56f3dbe2af1e43401f6b6c499cfd2c336f76c27
xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-01-09 13:15:37 +1300

    Refactored Inline-image component - Added image credit - Added Translations - Prose design tweaks

--- components/content/Media.vue
+++ components/content/Media.vue
@@ -1,10 +1,29 @@
 <template>
-    <NuxtImg :src="source" />
+    <figure>
+        <NuxtImg :src="source" />
+        <figcaption class="text-xs" v-bind:credit="credit">{{ t('Blog.source') + ": " + credit }}</figcaption>
+    </figure>
+
 </template>
-<script lang="ts">
-export default {
-    props: {
-        source: { type: String }
+<script setup lang="ts">
+const { t } = useI18n()
+
+defineProps({
+    source: { type: String },
+    credit: { type: String }
+})
+</script>
+<style>
+.prose figure figcaption {
+    color: #000;
+    font-size: 12px;
+    letter-spacing: 1px;
+    opacity: 0.7;
+}
+
+@media (prefers-color-scheme: dark) {
+    .dark .prose figure figcaption {
+        color: white;
     }
 }
-</script>
+</style>
