59b213f79d42dcdc239ac5bca4c6f7eea88ed3ae
xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-01-17 00:43:42 +1300

    Design Improvements - Updated PrevNext - Updated Social Share

--- components/Blog/SocialShare.vue
+++ components/Blog/SocialShare.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="lg:(sticky top-20) <sm:(ml-10 mt-5) mt-5">
-        <ul class="items-center lg:(space-y-4 flex flex-col items-center space-x-0) flex flex-row justify-left space-x-2 children:(p-2 lg:p-4 border-outline)">
+        <ul
+            class="items-center lg:(space-y-4 flex flex-col items-center space-x-0) flex flex-row justify-left space-x-2 children:(p-2 lg:p-4 border-outline accent-color bg-light-200 dark:bg-dark-900)">
             <li>
                 <NuxtLink
                     :to="`https://twitter.com/intent/tweet?url=https://xanzhu.com${post._path}&text=${post.title}&via=Xanzhu1`"
