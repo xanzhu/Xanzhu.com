a94d9564baaba5251204828aea41b1786602e2cc
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-02-23 17:01:00 +1300

    Test error page

---
+++ error.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dark:bg-black bg-white">
+    <div class="flex justify-center items-center h-screen">
+      <GlobalAppError />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  error: Object,
+});
+</script>
+
+<style>
+html {
+  background-color: #000;
+}
+</style>
