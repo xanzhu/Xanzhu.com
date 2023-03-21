fa7d0d28f647702d85ac8d5ffe56a6441e498844
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-03-21 21:56:55 +1300

    Removed dark border

--- components/Blog/SocialShare.vue
+++ components/Blog/SocialShare.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="py-2 flex">
+  <div class="flex">
     <div class="space-x-2">
       <NuxtLink
         :to="`https://twitter.com/intent/tweet?url=https://xanzhu.com${post._path}&text=${post.title}&via=Xanzhu1`"
         target="_blank"
-        class="dark:(bg-dark-900 border-dark-600 border-1) dark:hover:(bg-gradient-to-r text-white) rounded-sm from-red-600 to-[#ff0000] hover:text-[#ff0000] p-2"
+        class="hover:text-[#ff0000]"
       >
         <Icon name="mdi:twitter" class="h-5 w-5" />
       </NuxtLink>
       <NuxtLink
         :to="`https://www.linkedin.com/shareArticle?url=https://xanzhu.com${post._path}&title=${post.title}`"
         target="_blank"
-        class="dark:(bg-dark-900 border-dark-600 border-1) dark:hover:(bg-gradient-to-r text-white) rounded-sm from-red-600 to-[#ff0000] hover:text-[#ff0000] p-2"
+        class="hover:text-[#ff0000]"
       >
         <Icon name="mdi:linkedin" class="h-5 w-5" />
       </NuxtLink>
       <NuxtLink
         :to="`https://www.reddit.com/submit?url=https://xanzhu.com${post._path}&title=${post.title}`"
         target="_blank"
-        class="dark:(bg-dark-900 border-dark-600 border-1) dark:hover:(bg-gradient-to-r text-white) rounded-sm from-red-600 to-[#ff0000] hover:text-[#ff0000] p-2"
+        class="hover:text-[#ff0000]"
       >
         <Icon name="mdi:reddit" class="h-5 w-5" />
       </NuxtLink>
