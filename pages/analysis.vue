af86b00a6a18c1d15a70fc4a9971f4b9dec83205
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-03-21 19:37:19 +1300

    Fixed Open Graph Tags

--- pages/analysis.vue
+++ pages/analysis.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="flex justify-center items-center h-[70%]">
     <div class="flex flex-col items-center space-y-5 lg:max-w-lg xl:max-w-2xl">
-      <h1 class="text-5xl font-bold">{{ t("Analysis.h1") }}</h1>
+      <h1 class="text-5xl font-bold">{{ title }}</h1>
       <p
         class="dark:(bg-dark-900 border-dark-700 opacity-100) opacity-80 p-2 border-1 xl:max-w-lg"
       >
-        {{ t("Analysis.soon") }}
+        {{ desc }}
       </p>
     </div>
   </div>
 </template>
-<script setup>
-const { t } = useI18n();
+<script setup lang="ts">
+const { t, locale } = useI18n();
+
+const title = t("Analysis.h1");
+const desc = t("Analysis.soon");
 
 useSeoMeta({
-  title: t("Analysis.h1"),
-  description: t("Analysis.soon"),
+  title: title,
+  description: desc,
+  ogTitle: title,
+  ogDescription: desc,
+  twitterDescription: desc,
+  twitterTitle: title,
+  ogUrl: `https://xanzhu.com/${
+    locale.value === "en" ? "" : locale.value
+  }/analysis`,
+  ogImage: "https://source.unsplash.com/FlPc9_VocJ4",
 });
 </script>
