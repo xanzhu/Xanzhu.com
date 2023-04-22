2f97a525462de11fe2aba55bb376549c36bd1d34
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-04-23 00:27:38 +1200

    Fix Z position

--- components/AlertSoon.vue
+++ components/AlertSoon.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="z-30 max-w-md border-1 dark:text-white border-dark-300 rounded-md sm:right-4 mx-2 top-20 fixed p-5 text-sm backdrop-blur-lg backdrop-filter">
+        class="max-w-md dark:text-white rounded-md sm:right-4 mx2 top-20 fixed p5 text-sm backdrop-blur-lg backdrop-filter">
         <p>{{ $t('alert.maintenance') }}</p>
     </div>
 </template>
