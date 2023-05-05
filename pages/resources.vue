0ab347078d243f50f4c7eefcb5e13480dead7e0c
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-05-05 14:20:24 +1200

    Added h1

--- pages/resources.vue
+++ pages/resources.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="flex flex-col space-y-10 pt-10 ma px-5 sm:px-20 max-w-4xl">
   <div class="flex items-center justify-center sm:justify-between mb-10">
-    <div
+    <h1
       class="sm:text-center font-bold text-3xl sm:text-4xl break-words sm:max-w-md"
     >
     {{ $t("Resources.heading") }}
-    </div>
+    </h1>
     <div
       class="hidden md:flex rounded-full h-20 w-20 text-white items-center justify-center b-12 b-rounded-full b-dashed"
       :class="{'b-[#001EFA]':phishing, 'b-[#FF0000]':malware, 'b-white':a11y}"
