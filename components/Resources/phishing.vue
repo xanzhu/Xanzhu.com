763bb4038e98801e283d4f311f2b26601bea85e1
xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-06-02 15:50:47 +1200

    Resources: Use internal images

--- components/Resources/phishing.vue
+++ components/Resources/phishing.vue
@@ -1,48 +1,29 @@
 <template>
   <div class="grid sm:grid-cols-2 gap-4">
-    <div
-      class="sm:col-span-2 rounded-sm border-1 border-solid dark:border-brand-light b-transparent h-30"
-    >
-      <NuxtImg
-        src="https://source.unsplash.com/nJX3ceE4zuU"
-        class="h-full w-full rounded-sm object-cover"
-        alt="Paint Waves Graphic"
-      />
+    <div class="sm:col-span-2 rounded-sm border-1 border-solid dark:border-brand-light b-transparent h-30">
+      <NuxtImg src="/images/resources/phish.webp" class="h-full w-full rounded-sm object-cover"
+        alt="Paint Waves Graphic" />
     </div>
-    <div
-      class="p-4 font-bold bg-brand-light rounded-sm text-center text-white text-2xl"
-    >
+    <div class="p-4 font-bold bg-brand-light rounded-sm text-center text-white text-2xl">
       {{ $t("Resources.phishing") }}
     </div>
-    <div
-      class="dark:bg-dark-900 bg-light-600 b-1 b-solid rounded-sm border-brand-light p-4 row-span-2 col-span-1"
-    >
+    <div class="dark:bg-dark-900 bg-light-600 b-1 b-solid rounded-sm border-brand-light p-4 row-span-2 col-span-1">
       <ol class="m0 p2" v-for="url in urls" :key="url.name">
         <li
-          class="flex justify-center items-center space-x-1 op80 sm:text-md text-sm group hover:(underline decoration-1 underline-offset-2)"
-        >
-          <NuxtLink
-            :href="url.path"
-            :target="isExternalLink(url.path)"
-            rel="noopener"
-            class="no-underline text-md dark:text-white text-black op95"
-            >{{ url.name }}</NuxtLink
-          >
-          <Icon
-            v-if="isExternalLink(url.path) === '_blank'"
-            name="ri:external-link-line"
-            class="h-4 w-4"
-          />
+          class="flex justify-center items-center space-x-1 op80 sm:text-md text-sm group hover:(underline decoration-1 underline-offset-2)">
+          <NuxtLink :href="url.path" :target="isExternalLink(url.path)" rel="noopener"
+            class="no-underline text-md dark:text-white text-black op95">{{ url.name }}</NuxtLink>
+          <Icon v-if="isExternalLink(url.path) === '_blank'" name="ri:external-link-line" class="h-4 w-4" />
         </li>
       </ol>
     </div>
-    <div
-      class="dark:(bg-dark-900 op-80) bg-light-600 b-1 b-solid rounded-sm border-brand-light h-sm p-4 col-span-1"
-    >
-    <p class="font-medium items-center inline-flex ml-2">{{ $t('Resources.explain.title') }}<Icon class="h3.5 w3.5 ml-1" name=ooui:help-notice-ltr /></p>
-        <p class="px-5 m0">
-          {{ $t('Resources.explain.phishing') }}
-        </p>
+    <div class="dark:(bg-dark-900 op-80) bg-light-600 b-1 b-solid rounded-sm border-brand-light h-sm p-4 col-span-1">
+      <p class="font-medium items-center inline-flex ml-2">{{ $t('Resources.explain.title') }}
+        <Icon class="h3.5 w3.5 ml-1" name=ooui:help-notice-ltr />
+      </p>
+      <p class="px-5 m0">
+        {{ $t('Resources.explain.phishing') }}
+      </p>
     </div>
   </div>
 </template>
@@ -55,13 +36,13 @@ const isExternalLink = (url: string): "_blank" | "_self" => {
 }
 
 const urls = [
-      {
-        name: "Google Safe Browsing",
-        path: "https://safebrowsing.google.com/safebrowsing/report_phish/",
-      },
-      {
-        name: "Microsoft Site Reporting",
-        path: "https://www.microsoft.com/en-us/wdsi/support/report-unsafe-site-guest",
-      },
+  {
+    name: "Google Safe Browsing",
+    path: "https://safebrowsing.google.com/safebrowsing/report_phish/",
+  },
+  {
+    name: "Microsoft Site Reporting",
+    path: "https://www.microsoft.com/en-us/wdsi/support/report-unsafe-site-guest",
+  },
 ]
 </script>
--- pages/resources.vue
+++ pages/resources.vue
@@ -1,52 +1,38 @@
 <template>
-<div class="flex flex-col space-y-10 pt-10 ma px-5 sm:px-20 max-w-4xl">
-  <div class="flex items-center justify-center sm:justify-between mb-10">
-    <h1
-      class="sm:text-center font-bold text-3xl sm:text-4xl break-words sm:max-w-md"
-    >
-    {{ $t("Resources.heading") }}
-    </h1>
+  <div class="flex flex-col space-y-10 pt-10 ma px-5 sm:px-20 max-w-4xl">
+    <div class="flex items-center justify-center sm:justify-between mb-10">
+      <h1 class="sm:text-center font-bold text-3xl sm:text-4xl break-words sm:max-w-md">
+        {{ $t("Resources.heading") }}
+      </h1>
+      <div
+        class="hidden md:flex rounded-full h-20 w-20 text-white items-center justify-center b-12 b-rounded-full b-dashed"
+        :class="{ 'b-brand-light': phishing, 'b-brand-dark': malware, 'dark:b-white b-black': a11y }" />
+    </div>
     <div
-      class="hidden md:flex rounded-full h-20 w-20 text-white items-center justify-center b-12 b-rounded-full b-dashed"
-      :class="{'b-brand-light':phishing, 'b-brand-dark':malware, 'b-white':a11y}"
-    />
-  </div>
-  <div
-    class="flex flex-col space-y-5 sm:flex-row sm:space-x-5 sm:space-y-0 font-medium children:(dark:text-white text-black cursor-pointer b-1 b-solid rounded-sm px6 py2 font-bold)"
-  >
-  <!-- TODO: Add hover colours -->
-    <button
-      @click="toggle('phishing')"
-      class="hover:b-brand-light"
-      :class="{'bg-brand-light b-brand-light text-white': phishing, 'bg-transparent dark:b-white':!phishing}"
-    >
-    {{ $t("Resources.phishing") }}
-    </button>
-    <button
-      @click="toggle('a11y')"
-      class="dark:hover:b-white hover:b-black"
-      :class="{'dark:bg-white dark:!text-black dark:b-white b-black bg-black text-white': a11y, 'bg-transparent dark:b-white':!a11y}"
-    >
-    {{ $t("Resources.accessibility") }}
-    </button>
-    <button
-      @click="toggle('malware')"
-      class="hover:b-brand-dark"
-      :class="{'bg-brand-dark b-brand-dark !text-black': malware, 'bg-transparent dark:b-white':!malware}"
-    >
-    {{ $t("Resources.malware") }}
-    </button>
-  </div>
-  <ResourcesPhishing v-if="phishing && !malware && !a11y" />
-  <LazyResourcesMalware v-else-if="malware && !phishing && !a11y" />
-  <LazyResourcesAccessibility v-else-if="a11y && !phishing && !malware" />
+      class="flex flex-col space-y-5 sm:flex-row sm:space-x-5 sm:space-y-0 font-medium children:(dark:text-white text-black cursor-pointer b-1 b-solid rounded-sm px6 py2 font-bold)">
+      <!-- TODO: Add hover colours -->
+      <button @click="toggle('phishing')" class="hover:b-brand-light"
+        :class="{ 'bg-brand-light b-brand-light text-white': phishing, 'bg-transparent dark:b-white': !phishing }">
+        {{ $t("Resources.phishing") }}
+      </button>
+      <button @click="toggle('a11y')" class="dark:hover:b-white hover:b-black"
+        :class="{ 'dark:bg-white dark:!text-black dark:b-white b-black bg-black text-white': a11y, 'bg-transparent dark:b-white': !a11y }">
+        {{ $t("Resources.accessibility") }}
+      </button>
+      <button @click="toggle('malware')" class="hover:b-brand-dark"
+        :class="{ 'bg-brand-dark b-brand-dark !text-black': malware, 'bg-transparent dark:b-white': !malware }">
+        {{ $t("Resources.malware") }}
+      </button>
+    </div>
+    <ResourcesPhishing v-if="phishing && !malware && !a11y" />
+    <LazyResourcesMalware v-else-if="malware && !phishing && !a11y" />
+    <LazyResourcesAccessibility v-else-if="a11y && !phishing && !malware" />
 
-  <p class="text-center">{{ $t('Resources.soon') }}</p>
-</div>
-
+    <p class="text-center">{{ $t('Resources.soon') }}</p>
+  </div>
 </template>
 <script setup lang="ts">
-const {t, locale} = useI18n();
+const { t, locale } = useI18n();
 
 // Default option
 const phishing = ref(true);
