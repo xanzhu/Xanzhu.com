6d39e722e1646a2f152f6f963a0d89d156e277c8
xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-07-13 23:28:45 +1200

    Deps

--- uno.config.ts
+++ uno.config.ts
@@ -1,27 +1,33 @@
 // uno.config.ts
-import { defineConfig, presetUno, presetWebFonts, presetTypography } from 'unocss'
-import transformerVariantGroup from '@unocss/transformer-variant-group'
+import {
+  defineConfig,
+  presetIcons,
+  presetWebFonts,
+  presetTypography,
+} from "unocss";
+import presetUno from "@unocss/preset-uno";
+import transformerVariantGroup from "@unocss/transformer-variant-group";
+import transformerDirectives from "@unocss/transformer-directives";
 
 export default defineConfig({
-    presets: [
-        presetUno(),
-        presetTypography(),
-        presetWebFonts({
-            provider: 'fontshare',
-            fonts: {
-              sans: 'Satoshi',
-            },
-          }),
-    ],
-    transformers: [
-        transformerVariantGroup(),
-    ],
-    theme: {
-      colors: {
-        'brand': {
-          'dark': '#ff0000', // text-brand-dark
-          'light': '#001EFA' // text-brand-light
-        }
-      }
-    }
-})
+  presets: [
+    presetUno(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      provider: "google",
+      fonts: {
+        sans: "Noto Sans KR",
+      },
+    }),
+  ],
+  transformers: [transformerVariantGroup(), transformerDirectives()],
+  theme: {
+    colors: {
+      brand: {
+        dark: "#ff0000", // text-brand-dark
+        light: "#001EFA", // text-brand-light
+      },
+    },
+  },
+});
