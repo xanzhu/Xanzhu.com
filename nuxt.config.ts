1b6e4b98c73a5d5dba1a93227e1e76d7c1d17a0c
xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-11-07 00:20:28 +1300

    Move locales to assets folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
     defaultLocale: "en",
     lazy: true,
     strategy: "prefix_except_default",
-    langDir: "locales",
+    langDir: "assets/locales",
     locales: [
       {
         code: "en",
