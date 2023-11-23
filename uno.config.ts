7b5ae279141c5369d66aa8f29d881abb110bc737
xanzhu <34955679+xanzhu@users.noreply.github.com>
2023-11-23 19:58:55 +1300

    Updated Accent Colour Red to blue!

--- uno.config.ts
+++ uno.config.ts
@@ -5,16 +5,13 @@ import transformerVariantGroup from "@unocss/transformer-variant-group";
 import transformerDirectives from "@unocss/transformer-directives";
 
 export default defineConfig({
-  presets: [
-    presetUno(),
-    presetTypography(),
-  ],
+  presets: [presetUno(), presetTypography()],
   transformers: [transformerVariantGroup(), transformerDirectives()],
   theme: {
     colors: {
       brand: {
-        dark: "#ff0000", // text-brand-dark //
-        light: "#d51600", // text-brand-light // #b0100d // #B00F0C (AAA)
+        dark: "#007BFF", // text-brand-dark
+        light: "#0067d4", // text-brand-light
       },
     },
   },
