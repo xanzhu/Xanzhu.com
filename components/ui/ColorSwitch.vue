081f29925af949198b824aaf4cb8f787aa82287e
xanzhu <34955679+xanzhu@users.noreply.github.com>
2024-01-13 11:25:16 +1300

    Improve Icon Performance - Testing impact

--- components/ui/ColorSwitch.vue
+++ components/ui/ColorSwitch.vue
@@ -1,12 +1,12 @@
 <template>
   <button @click="switchColor" class="bg-transparent b-hidden flex inline-block cursor-pointer mr0"
     aria-label="Color Switcher">
-    <Icon v-if="$colorMode.preference === 'dark'" class="h4.5 w4.5 dark:text-white text-black" name="uil:sun" />
-    <Icon v-else class="h5 w5 text-black dark:text-white" name="uil:moon" />
+    <Icon v-if="$colorMode.preference === 'dark'" class="h4.5 w4.5 dark:text-black text-white" name="uil:sun" />
+    <Icon v-else class="h5 w5 text-white dark:text-black" name="uil:moon" />
   </button>
 </template>
 <script setup lang="ts">
-const c = useColorMode();
+let c = useColorMode();
 
 function switchColor() {
   c.preference = c.value === "dark" ? "light" : "dark";
