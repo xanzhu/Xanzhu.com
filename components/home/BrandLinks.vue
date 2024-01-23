3c3d233a2ccb87c5220437fdfbc1117b6defd6ad
xanzhu <34955679+xanzhu@users.noreply.github.com>
2024-01-23 13:00:43 +1300

    Sizing tweak

--- components/home/BrandLinks.vue
+++ components/home/BrandLinks.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="mx-2 p1 justify-center space-y-4 mt2 flex flex-col items-center justify-center text-xs font-medium text-center xl:(px5 py2) sm:(flex-row justify-between max-w-[38.5rem] space-y-0 mx-auto) md:(max-w-[46.5rem] px3 py1.5) lg:(max-w-[62.5rem]) children:(dark:(bg-dark-900 text-white b-dark-700) bg-light-400 text-black px4 py1.5 rounded-sm b-1 b-solid b-gray-300)">
+        class="mx-2 p1 justify-center space-y-4 mt2 flex flex-col items-center justify-center text-xs font-medium text-center xl:(px5 py2) sm:(flex-row justify-between max-w-[38.5rem] space-y-0 mx-auto) md:(max-w-[46.5rem] px3 py1.5) lg:(max-w-[62.5rem]) xl:(max-w-[72.5rem]) children:(dark:(bg-dark-900 text-white b-dark-700) bg-light-400 text-black px4 py1.5 rounded-sm b-1 b-solid b-gray-300)">
         <div class="children:(decoration-none dark:text-white text-black) space-x-4">
             <NuxtLink
                 class="group sm:inline-flex hover:(decoration-2 decoration-brand-light dark:decoration-brand-dark underline underline-offset-3) p0 items-center"
