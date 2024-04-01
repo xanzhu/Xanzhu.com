96520042d12267dd367a18e49a5b1ca93e335f82
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2024-04-01 17:13:35 +1300

    Fix: Incorrect Weather on locale Switch

--- components/ui/Weather.vue
+++ components/ui/Weather.vue
@@ -1,7 +1,7 @@
 <template>
-    <div v-if="weatherData" class="flex items-center space-x-2 text-md font-medium">
+    <div v-if="weatherData" class="flex items-center space-x-2 text-md">
         <NuxtImg :src="'https:' + weatherData.current.condition.icon" alt="weather icon" height="32" width="32"
-            class="h-8 w-auto filter saturate-700" />
+            class="h-8 w-auto" />
         <p>{{ weatherData.location.name }}</p>
         <p>{{ weatherData.current.temp_c }}°C</p>
         <p>{{ weatherData.current.condition.text }}</p>
@@ -10,17 +10,27 @@
         <p class="animate-flash text-md">{{ $t('Home.weatherLoad') }}</p>
     </div>
 </template>
-
 <script setup>
-import { getWeather } from '~/lib/weather.js';
+import { getWeather } from '~/lib/w.js';
 
 const weatherData = ref(null);
+const { locale } = useI18n();
 
-onMounted(async () => {
+const fetchWeatherData = async (newLocale) => {
     try {
-        weatherData.value = await getWeather();
+        weatherData.value = await getWeather(null, newLocale);
     } catch (error) {
         console.error('Error fetching weather data:', error);
     }
+};
+
+watch(() => locale.value, (newLocale, oldLocale) => {
+    if (newLocale !== oldLocale) {
+        fetchWeatherData(newLocale);
+    }
+});
+
+onMounted(() => {
+    fetchWeatherData(locale.value);
 });
 </script>
