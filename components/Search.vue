5b3ce5cb2053e41498076ff17ca8450552c241be
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2024-06-24 22:47:18 +1200

    Design Testing

--- components/Search.vue
+++ components/Search.vue
@@ -1,22 +1,30 @@
 <template>
-    <main class="p-4 rounded-md overflow-hidden">
-        <div class="relative">
+    <main class="p-4 rounded-md overflow-hidden dark:bg-gray-800">
+        <div class="relative mb-4">
             <input v-model="search" @input="debouncedSearch" type="search" id="default-search"
-                placeholder="Search Articles.." required />
+                placeholder="Search Articles.." required
+                class="w-full px-3 py-2 border rounded-md focus:outline-none dark:focus:ring-2 dark:focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600" />
         </div>
-        <ul v-if="searchResults.length && !isLoading" class="list-none">
-            <li v-for="result in searchResults" :key="result.id" class="">
-                <a :href="result.id" class="">
-                    <div class="">
-                        <h3 class="">{{ result.title }}</h3>
-                        <div class="" v-html="result.highlightedContent">
-                        </div>
+        <div v-if="isLoading" class="font-bold text-sm text-blue-700 uppercase dark:text-blue-500">
+            Searching for "{{ search }}"..
+        </div>
+        <div v-if="!isLoading && searchResults.length > 0"
+            class="font-bold text-sm text-blue-700 uppercase dark:text-blue-500">
+            Found {{ count }} results for "{{ search }}"
+        </div>
+        <ul v-if="searchResults.length && !isLoading" class="list-none p-0">
+            <li v-for="result in searchResults" :key="result.id" class="border-b last:border-b-0 dark:border-gray-600">
+                <a :href="result.id" class="block hover:bg-gray-100 px-4 py-3 dark:hover:bg-gray-600 no-underline">
+                    <div>
+                        <h3 class="font-bold text-xl dark:text-white text-black">{{ result.title }}</h3>
+                        <div class="font-semibold text-lg text-black dark:text-white"
+                            v-html="result.highlightedContent"></div>
                     </div>
                 </a>
             </li>
         </ul>
         <div v-else-if="!isLoading && searchInitiated && searchResults.length === 0 && search"
-            class="mt-4">
+            class="font-bold text-sm text-blue-700 uppercase dark:text-blue-500">
             No results found for "{{ search }}"
         </div>
     </main>
@@ -111,6 +119,8 @@ const highlightMatch = (content: string | undefined): string => {
 
     return snippet;
 };
+
+const count = computed(() => searchResults.value.length);
 
 watch([search, () => locale.value], () => {
     debouncedSearch();
