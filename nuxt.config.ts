da61487948d3b96da531bfdb680081af5c07f309
xanzhu <34955679+xanzhu@users.noreply.github.com>
2024-07-21 19:04:14 +1200

    Cloudflare Test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,8 @@ export default defineNuxtConfig({
     "nuxt-icon",
     "nuxt-simple-robots",
     "@nuxt/image",
+    "nitro-cloudflare-dev",
+    "nuxt-security",
   ],
 
   colorMode: {
@@ -69,7 +71,9 @@ export default defineNuxtConfig({
     "/blog/**": { isr: true },
   },
 
-  plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
+  nitro: {
+    preset: "cloudflare-pages",
+  },
 
   image: {
     domains: ["source.unsplash.com"],
@@ -85,6 +89,55 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       WEATHER_API: process.env.WEATHER_API,
+    },
+  },
+
+  security: {
+    nonce: true,
+    ssg: {
+      meta: true, // Enables CSP as a meta tag in SSG mode
+      hashScripts: true, // Enables CSP hash support for scripts in SSG mode
+      hashStyles: false, // Disables CSP hash support for styles in SSG mode (recommended)
+    },
+    headers: {
+      crossOriginEmbedderPolicy:
+        process.env.NODE_ENV === "development" ? "unsafe-none" : "unsafe-none",
+      contentSecurityPolicy: {
+        "script-src": [
+          "'self'", // Fallback value, will be ignored by most modern browsers (level 3)
+          "https:", // Fallback value, will be ignored by most modern browsers (level 3)
+          "'unsafe-inline'", // Fallback value, will be ignored by almost any browser (level 2)
+          "'strict-dynamic'", // Strict CSP via 'strict-dynamic', supported by most modern browsers (level 3)
+          "'nonce-{{nonce}}'", // Enables CSP nonce support for scripts in SSR mode, supported by almost any browser (level 2)
+        ],
+        "style-src": [
+          "'self'", // Enables loading of stylesheets hosted on same origin
+          "https:", // For increased security, replace by the specific hosting domain or file name of your external stylesheets
+          "'unsafe-inline'", // Recommended default for most Nuxt apps
+        ],
+        "base-uri": ["'none'"],
+        "img-src": [
+          "'self'",
+          "data:",
+          "https://web-platforms.sfo2.digitaloceanspaces.com/WWW/Badge%203.svg",
+        ],
+        "font-src": ["'self'", "https:", "data:"],
+        "object-src": ["'none'"],
+        "script-src-attr": ["'none'"],
+        "upgrade-insecure-requests": true,
+        "connect-src": [
+          "'self'",
+          "https://web-platforms.sfo2.digitaloceanspaces.com",
+          "https://api.weatherapi.com",
+        ],
+      },
+      strictTransportSecurity: {
+        includeSubdomains: true,
+        maxAge: 63072000,
+        preload: true,
+      },
+      crossOriginResourcePolicy: "cross-origin",
+      crossOriginOpenerPolicy: "unsafe-none",
     },
   },
 });
