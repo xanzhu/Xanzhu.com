00a16df98504c9a05862e2c71ff4011b4320c4c0
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2024-07-23 22:08:35 +1200

    Test Strict Security Implementation - Temp disable Vercel.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
     "@nuxtjs/robots",
     "@nuxt/image",
     "nuxt-link-checker",
+    "nuxt-security"
   ],
 
   colorMode: {
@@ -108,6 +109,39 @@ export default defineNuxtConfig({
         "fluent:local-language-16-filled",
       ],
     },
+  },
+
+  // SECURITY V1
+   security: {
+    nonce: true, // Enables HTML nonce support in SSR mode
+    ssg: {
+      meta: true, // Enables CSP as a meta tag in SSG mode
+      hashScripts: true, // Enables CSP hash support for scripts in SSG mode
+      hashStyles: false // Disables CSP hash support for styles in SSG mode (recommended)
+    },
+    headers: {
+      contentSecurityPolicy: {
+        'script-src': [
+          "'self'",  // Fallback value, will be ignored by most modern browsers (level 3)
+          "https:", // Fallback value, will be ignored by most modern browsers (level 3)
+          "'unsafe-inline'", // Fallback value, will be ignored by almost any browser (level 2)
+          "'strict-dynamic'", // Strict CSP via 'strict-dynamic', supported by most modern browsers (level 3)
+          "'nonce-{{nonce}}'" // Enables CSP nonce support for scripts in SSR mode, supported by almost any browser (level 2)
+        ],
+        'style-src': [
+          "'self'", // Enables loading of stylesheets hosted on same origin
+          "https:", // For increased security, replace by the specific hosting domain or file name of your external stylesheets
+          "'unsafe-inline'" // Recommended default for most Nuxt apps
+        ],
+        'base-uri': ["'none'"],
+        'img-src': ["'self'", "data:", "https://web-platforms.sfo2.digitaloceanspaces.com/WWW/Badge%203.svg"], // Add relevant https://... sources if you load images from external sources 
+        'font-src': ["'self'", "https:", "data:"], //  For increased security, replace by the specific sources for fonts
+        'object-src': ["'none'"],
+        'script-src-attr': ["'none'"],
+        'upgrade-insecure-requests': true
+      }
+    },
+    sri: true
   },
 
   compatibilityDate: "2024-07-08",
