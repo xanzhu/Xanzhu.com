980d91499552971ad0c7058bc07027ce73f14f2f
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2024-08-19 17:02:56 +1200

    Updated TOC design - Better visual support - Fixed light version

--- components/Blog/Toc.vue
+++ components/Blog/Toc.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="m-4 p-4 rounded-md dark:(bg-dark-900 b-dark-600) b-gray-300 b-1 b-solid md:(max-w-md mx-auto)">
+  <nav class="m-4 p-4 rounded-md dark:(bg-dark-900 b-dark-600) bg-light6 b-gray-300 b-1 b-solid md:(max-w-md mx-auto)">
     <header class="pb-2 mb-2 border-b b-slate-200 dark:b-dark-300">
       <p class="text-lg font-semibold m0 text-center">
         {{ t("Blog.toc") }}
@@ -7,11 +7,9 @@
     </header>
     <ul class="flex flex-col gap-2 px-6 text-sm">
       <li v-for="link of flattenLinks(links)" :key="link.id" class="text-gray-500 dark:text-light-400" :class="{
-        'ml-4 list-disc opacity-80 hover:(underline underline-offset-3 decoration-2)':
-          link.depth === 3,
-        'font-semibold tracking-wide  hover:underline-transparent':
-          link.depth === 2,
-      }">
+          'ml-4 list-disc opacity-80 hover:(underline underline-offset-3 decoration-2 decoration-brand-light)': link.depth === 3,
+          'font-semibold list-none -ml4 mr-auto py1 px3 rounded-md bg-light8 dark:bg-dark7': link.depth === 2,
+        }">
         <NuxtLink class="no-underline dark:text-white text-black" :href="`#${link.id}`">
           {{ link.text }}
         </NuxtLink>
