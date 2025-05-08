0ebdeae27956bfe4131b6239a6769a0fbad1ca5f
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2025-05-08 12:58:10 +1200

    chore: deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -87,7 +87,7 @@ export default defineNuxtConfig({
   // API
   runtimeConfig: {
     public: {
-      Version: '1.0.120',
+      Version: '1.0.131',
       WeatherAPI: process.env.WEATHER_API,
       i18n: {
         baseUrl: 'https://xanzhu.com',
