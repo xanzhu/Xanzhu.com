46bf235a7257bf863f9ec2ea8b271dcd6e250687
xanzhu <34955679+xanzhu@users.noreply.github.com>
2025-05-19 17:37:51 +1200

    Content: Sizing fix

--- app/components/content/Media.vue
+++ app/components/content/Media.vue
@@ -9,13 +9,15 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <figure class="w-full flex flex-col object-cover">
-    <NuxtImg
-      class="h-auto max-w-full rounded-lg bg-black core-border dark:bg-white" :src="source" :alt="alt"
-      :title="alt" loading="lazy" decoding="async"
-    />
-    <figcaption v-if="credit" :credit="credit">
-      {{ `${t("Blog.source")}: ${credit}` }}
+  <figure class="w-full flex flex-col">
+    <div class="w-full overflow-hidden border border-neutral-200 rounded-lg dark:border-neutral-800">
+      <NuxtImg
+        :src="source" :alt="alt" :title="alt" loading="lazy" decoding="async"
+        class="h-auto max-w-full w-full rounded-lg object-contain"
+      />
+    </div>
+    <figcaption v-if="credit" class="mt-2 text-sm text-neutral-600 dark:text-neutral-400">
+      {{ `${t('Blog.source')}: ${credit}` }}
     </figcaption>
   </figure>
 </template>
