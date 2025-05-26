326c29901584488483b09a104e55283d2aa02124
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2025-05-26 12:56:31 +1200

    Test ColorSwitch changes

--- app/components/ui/ColorSwitch.vue
+++ app/components/ui/ColorSwitch.vue
@@ -11,22 +11,15 @@ function switchColor() {
 </script>
 
 <template>
-  <div class="hidden">
-    <Icon name="line-md:moon-filled-alt-to-sunny-filled-loop-transition" />
-    <Icon name="line-md:moon-filled-alt-loop" />
-  </div>
   <button
-    class="mr-0 inline-block flex cursor-pointer border-none bg-transparent" aria-label="Color Switcher"
+    class="mr-0 inline-block flex w-8 cursor-pointer items-center justify-center border-none bg-transparent" aria-label="Color Switcher"
     @click="switchColor"
   >
-    <Icon :name="colorIcon" class="h-6 w-6 text-black transition-all duration-500 dark:text-white" />
+    <ClientOnly>
+      <Icon :name="colorIcon" class="h-full w-full text-black transition-all duration-500 dark:text-white" />
+      <template #fallback>
+        <div class="h-full w-full" />
+      </template>
+    </ClientOnly>
   </button>
 </template>
-
-<style scoped>
-button {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-</style>
