a1a14bede46da973d040ba2c79bcab21f262eb48
xanzhu <34955679+xanzhu@users.noreply.github.com>
2025-07-25 19:42:37 +1200

    UI: Improved layout - Refactored i18n keys - Improved a11y

--- app/components/MobileNav.vue
+++ app/components/MobileNav.vue
@@ -17,44 +17,38 @@ const availableLocales = computed(() => {
 </script>
 
 <template>
-  <div class="flex items-center text-center space-x-3">
+  <div class="flex items-center text-center space-x-1">
     <UiColorSwitch />
-    <Icon
-      name="fluent:local-language-16-filled" class="h6 w6 cursor-pointer" :aria-label="t('app.sr.lang')"
-      :aria-expanded="isLangOpen ? 'true' : 'false'" aria-controls="language-options" @click="toggle('language')"
-    >
-      <span class="sr-only">{{ t('app.sr.lang') }}</span>
-    </Icon>
-    <div
-      id="language-options"
-      :class="isActive('language') ? 'flex flex-col absolute top-13 core-ui core-border rounded-md p2 shadow-md right-13 children:(mb1) w-20' : 'hidden'"
-    >
-      <NuxtLink
-        v-for="locale in availableLocales" :key="locale.code"
-        :aria-label="t('app.sr.lang_select') + locale.name" :to="switchLocalePath(locale.code)"
+    <button
+      class="cursor-pointer border border-1 border-transparent rounded-md bg-transparent px1.5 py0.5 pt1 text-black hover:(core-border core-ui) dark:text-white"
+      @click="toggle('language')" :aria-expanded="isLangOpen ? 'true' : 'false'" aria-controls="language-options">
+      <Icon name="fluent:local-language-16-filled" class="h6 w6" aria-hidden="true" />
+      <span class="sr-only">{{ t('v2.langSwitch.select') }}</span>
+    </button>
+    <div v-show="isLangOpen" id="language-options"
+      class="absolute right-12 top-13 w-25 flex flex-col core-border rounded-md core-ui p2 shadow-md children:(mb1)"
+      tabindex="-1" role="menu" @keydown.esc="toggle('language')">
+      <NuxtLink v-for="locale in availableLocales" :key="locale.code"
+        :to="switchLocalePath(locale.code)"
         class="h-5 b-1 b-transparent rounded-md b-solid p2 text-inherit no-underline hover:(core-border core-theme)"
         exact-active-class="dark:bg-black bg-white pointer-events-none order-first !core-border"
-        @click="toggle('language')"
-      >
+        @click="toggle('language')" role="menuitem" :aria-current="locale.code === $i18n.locale ? 'true' : undefined"
+        :aria-label="locale.code === $i18n.locale ? `${locale.name} ${t('v2.langSwitch.current')}` : `${t('v2.langSwitch.switch')} ${locale.name}`">
         {{ locale.name }}
       </NuxtLink>
     </div>
-    <Icon
-      name="fluent:list-rtl-16-filled" class="h6 w6 cursor-pointer"
-      :aria-expanded="isMenuOpen ? 'true' : 'false'" aria-controls="menu-options" @click="toggle('menu')"
-    >
+    <button
+      class="cursor-pointer border border-1 border-transparent rounded-md bg-transparent px1.5 py0.5 pt1 text-black hover:(core-border core-ui) dark:text-white"
+      @click="toggle('menu')" :aria-expanded="isMenuOpen ? 'true' : 'false'" aria-controls="menu-options">
+      <Icon name="fluent:list-rtl-16-filled" class="h6 w6" aria-hidden="true" />
       <span class="sr-only">{{ t('app.sr.menu') }}</span>
-    </Icon>
-    <nav
-      id="menu-options"
-      :class="isActive('menu') ? 'flex flex-col absolute top-13 core-ui core-border rounded-md p2 shadow-md right-4 children:(mb1) min-w-25' : 'hidden'"
-      role="menu"
-    >
-      <NuxtLinkLocale
-        v-for="(link, index) in links" :key="index" :to="link.url" class="h-5 b-1 b-transparent rounded-md b-solid p2 text-black no-underline hover:(core-border core-theme) dark:(text-white)"
-        active-class="dark:bg-black bg-white pointer-events-none order-first !core-border"
-        @click="toggle('menu')"
-      >
+    </button>
+    <nav v-show="isMenuOpen" id="menu-options"
+      class="absolute right-2 top-13 min-w-25 flex flex-col core-border rounded-md core-ui p2 shadow-md children:(mb1)"
+      tabindex="-1" role="menu" @keydown.esc="toggle('menu')">
+      <NuxtLinkLocale v-for="(link, index) in links" :key="index" :to="link.url"
+        class="h-5 b-1 b-transparent rounded-md b-solid p2 text-black no-underline hover:(core-border core-theme) dark:(text-white)"
+        active-class="dark:bg-black bg-white pointer-events-none order-first !core-border" @click="toggle('menu')">
         {{ t(link.name) }}
       </NuxtLinkLocale>
     </nav>
