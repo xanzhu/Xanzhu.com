07d488ac65b7470ccc6caa2f97132b030d1807bb
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2025-07-29 22:26:44 +1200

    Fixes

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Xanzhu Security, helping improve privacy, accessiblity and security of
 technology we use. Redefining what is the standard and going beyond.
 
-Production build: 
+Production build:
 - https://xanzhu.com [Cloudflare]
 - https://prod.xanzhu.com [Vercel]
--- app/components/ui/Weather.vue
+++ app/components/ui/Weather.vue
@@ -45,18 +45,9 @@ onMounted(() => {
 
 <template>
   <div
-    <<<<<<< HEAD
-    class="flex items-center children:m0 space-x-2" :style="{
-      height: '30px',
-      opacity: weatherData ? 1 : 0,
-      transition: 'opacity 0.3s ease',
-    }"
-    =="====="
     class="h-[30px] flex items-center transition-opacity duration-300 ease-linear children:m-0 space-x-2"
     :class="weatherData ? 'opacity-100' : 'opacity-0'"
   >
-    >>>>>> developer
-    >
     <template v-if="weatherData">
       <p>{{ weatherData.location.name }}</p>
       <p class="font-bold">
