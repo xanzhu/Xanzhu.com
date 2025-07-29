c5b8df4ddf5682202f6f2d10dd3d8e309a4adaa5
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2025-07-29 22:48:44 +1200

    chore: bump version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -114,7 +114,7 @@ export default defineNuxtConfig({
   // API
   runtimeConfig: {
     public: {
-      Version: '1.0.210',
+      Version: '1.0.211',
       WeatherAPI: process.env.WEATHER_API,
       i18n: {
         baseUrl: 'https://xanzhu.com',
