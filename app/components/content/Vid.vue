a056dad923f24e111266e714f8174471a3854700
xanzhu <34955679+xanzhu@users.noreply.github.com>
2025-10-09 14:58:31 +1300

    Hotfix: Styling fixes - Regression from Build 1.0.410 q.q

--- app/components/content/Vid.vue
+++ app/components/content/Vid.vue
@@ -9,15 +9,23 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <figure>
+  <figure class="m-y-6">
     <video
-      class="core-border rounded-md rounded-md" muted :autoplay="true" :loop="true" :controls="false" playsinline
+      class="block h-auto max-w-full w-full core-border rounded-md object-cover"
+      muted
+      autoplay
+      loop
+      playsinline
       :poster="thumbnail"
     >
       <source :src="source">
     </video>
-    <figcaption v-if="credit" :credit="credit">
-      {{ t("Blog.source") }}{{ `: ${credit}` }}
+
+    <figcaption
+      v-if="credit"
+      class="mt-1 text-left text-xs text-gray-600 leading-relaxed font-mono opacity-85 dark:text-light-900"
+    >
+      {{ t('Blog.source') }}{{ `: ${credit}` }}
     </figcaption>
   </figure>
 </template>
