ef2116061d67dafbe64a0be65226aea114760fe5
xanzhu <34955679+xanzhu@users.noreply.github.com>
2025-10-24 12:48:36 +1300

    chore: deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -118,7 +118,7 @@ export default defineNuxtConfig({
   // API
   runtimeConfig: {
     public: {
-      Version: '2.0.50',
+      Version: '2.0.51',
       i18n: {
         baseUrl: 'https://xanzhu.com',
       },
