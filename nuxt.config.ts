e4713cd7c2f49920aa70fe6db381eb8fadb087ee
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2025-11-03 00:03:54 +1300

    chore: version bump

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -118,7 +118,7 @@ export default defineNuxtConfig({
   // API
   runtimeConfig: {
     public: {
-      Version: '2.0.71',
+      Version: '2.1.0',
       i18n: {
         baseUrl: 'https://xanzhu.com',
       },
