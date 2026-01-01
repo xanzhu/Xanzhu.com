c1ef6fbd40b6fe5c2c02c3b7d8adcfd802751786
xanzhu <34955679+xanzhu@users.noreply.github.com>
2026-01-01 16:14:04 +1300

    Minor tweak

--- app/components/Blog/BlogReadTime.vue
+++ app/components/Blog/BlogReadTime.vue
@@ -3,6 +3,8 @@ const props = defineProps<{
   content: object
 }>()
 
+const { t } = useI18n()
+
 const wordsPerMinute = 230
 
 const contentString = JSON.stringify(props.content)
@@ -17,7 +19,7 @@ const readingTime = Math.ceil(words / wordsPerMinute)
 <template>
   <div class="flex items-center gap-1">
     <p class="m0">
-      {{ $t("ui.content.readTime", { count: readingTime }) }}
+      {{ t("ui.content.readTime", { count: readingTime }) }}
     </p>
   </div>
 </template>
