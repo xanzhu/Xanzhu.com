f87d3ac23a17fb74ea6740f9835524b5edeee43a
xanzhu <34955679+xanzhu@users.noreply.github.com>
2026-02-10 20:43:51 +1300

    fix: removed sitemap nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -271,7 +271,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: true,
-      routes: ['/', '/blog', '/ko', '/zh', '/sitemap.xml'],
+      routes: ['/', '/blog', '/ko', '/zh'],
     },
     minify: !isDev,
     compressPublicAssets: true,
