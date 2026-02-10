4ac37ab71e52dd0c215d928307ba3667b2862c08
xanzhu <34955679+xanzhu@users.noreply.github.com>
2026-02-10 20:47:45 +1300

    fix: disable experimental feature

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -300,7 +300,7 @@ export default defineNuxtConfig({
   // Experimental Vue Features
   experimental: {
     extractAsyncDataHandlers: true,
-    payloadExtraction: true,
+    // payloadExtraction: true,
   },
 
   vite: {
