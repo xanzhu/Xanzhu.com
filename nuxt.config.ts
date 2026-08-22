038bfcaa81a0a9a1626229b3990053633b82c1d9
Xanzhu <34955679+xanzhu@users.noreply.github.com>
2026-08-22 22:20:38 +1200

    cf worker tweaks - ISR reset - config changes - caching

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -113,16 +113,14 @@ export default defineNuxtConfig({
   },
 
   routeRules: {
-    '/': { isr: 86400 },
     '/**': {
       headers: {
         'x-robots-tag': 'index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1',
-        'Cache-Control': 'public, max-age=3600, s-maxage=86400, stale-while-revalidate=86400',
+        'Cache-Control': 'public, max-age=3600, stale-while-revalidate=86400',
       },
     },
     '/**/_payload.json': {
       prerender: false,
-      ssr: false,
     },
     '/_nuxt/**': {
       headers: {
@@ -131,21 +129,14 @@ export default defineNuxtConfig({
     },
     '/images/**': {
       headers: {
-        'Cache-Control': 'public, max-age=2592000, immutable',
+        'Cache-Control': 'public, max-age=2592000, stale-while-revalidate=86400, immutable',
       },
     },
     '/health': {
       security: { headers: false },
       prerender: false,
+      headers: { 'Cache-Control': 'no-store' },
     },
-    '/blog/**': { isr: 86400 },
-    '/ko/blog/**': { isr: 86400 },
-    '/zh/blog/**': { isr: 86400 },
-    '/resources': { isr: 86400 },
-    '/ko/resources': { isr: 86400 },
-    '/zh/resources': { isr: 86400 },
-    // Testing
-    '/sitemap.xml': { redirect: { to: '/sitemap_index.xml', statusCode: 301 } },
   },
 
   content: {
@@ -181,15 +172,15 @@ export default defineNuxtConfig({
     },
   },
 
-  // SECURITY V2.1
+  // SECURITY
   security: {
     nonce: true,
+    corsHandler: false,
     ssg: {
       meta: true,
       hashScripts: true,
       hashStyles: true,
     },
-    corsHandler: false,
     headers: {
       contentSecurityPolicy: {
         'default-src': ['\'self\'', 'https://*.xanzhu.com'],
@@ -271,9 +262,9 @@ export default defineNuxtConfig({
     sri: true,
   },
 
-  compatibilityDate: '2026-06-23',
+  compatibilityDate: '2026-08-20',
 
-  sourcemap: isDev,
+  // sourcemap: isDev,
 
   unocss: {
     disableNuxtInlineStyle: false,
@@ -288,18 +279,13 @@ export default defineNuxtConfig({
     prerender: {
       crawlLinks: true,
     },
+
     minify: !isDev,
     compressPublicAssets: true,
-  },
-
-  $env: {
+    preset: 'cloudflare_module',
     cloudflare: {
-      nitro: {
-        preset: 'cloudflare_module',
-        cloudflare: {
-          deployConfig: true,
-        },
-      },
+      deployConfig: true,
+      nodeCompat: true,
     },
   },
 
@@ -307,8 +293,6 @@ export default defineNuxtConfig({
   experimental: {
     extractAsyncDataHandlers: true,
     payloadExtraction: 'client',
-    // Testing - 23/07
-    ssrStreaming: true,
   },
 
   vite: {
@@ -316,7 +300,11 @@ export default defineNuxtConfig({
       cssMinify: 'lightningcss',
     },
     optimizeDeps: {
-      include: ['@vue/devtools-core', '@vue/devtools-kit', '@vueuse/core'],
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+        '@vueuse/core',
+      ],
     },
   },
 
